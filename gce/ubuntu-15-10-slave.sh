d90f95bbc8df074feef8f1a9eeacbc353a78ea2a
Damien Martin-Guillerez <dmarting@google.com>
2016-09-09 21:38:52 +0200

    Add xfvb to the slaves
    web_test rules use it to simulate X window so that it can run browser in it.
    
    Change-Id: I2310d02923506247c8af2063233d47a624e6a674

--- gce/ubuntu-15-10-slave.sh
+++ gce/ubuntu-15-10-slave.sh
@@ -16,7 +16,7 @@
 
 apt-get update
 apt-get install -y zip g++ zlib1g-dev wget git unzip python python3 curl \
-        openjdk-8-jdk openjdk-8-source ca-certificates-java
+        openjdk-8-jdk openjdk-8-source ca-certificates-java xfvb
 
 # Android SDK requires 32-bits libraries
 sudo dpkg --add-architecture i386
