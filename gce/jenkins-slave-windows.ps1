213e75085f9b5c33da77a0b9b4fa0dac84a10021
Yun Peng <pcloudy@google.com>
2017-01-30 15:35:58 +0100

    Install python package pyreadline on Windows salve
    Required by TensorFlow Python tests
    
    Change-Id: I6830419b7b24e7c392d317158608906fb50e07ea

--- gce/jenkins-slave-windows.ps1
+++ gce/jenkins-slave-windows.ps1
@@ -48,6 +48,10 @@ $anaconda3_installer = $anaconda3_tmp_folder + "\Anaconda3-4.2.0-Windows-x86_64.
 (New-Object Net.WebClient).DownloadFile("https://repo.continuum.io/archive/Anaconda3-4.2.0-Windows-x86_64.exe", $anaconda3_installer)
 & $anaconda3_installer /AddToPath=0? /InstallationType=AllUsers /S /D='C:\Program Files\Anaconda3'
 
+# Install pyreadline (Windows-compatible Python GNU readline library).
+# Required by TensorFlow Python tests (tfdbg).
+& "C:\Program Files\Anaconda3\Scripts\pip.exe" install pyreadline
+
 # Install all the Windows software we need:
 #   - JDK, because, Bazel is written in Java
 #   - NSSM, because that's the easiest way to create services
