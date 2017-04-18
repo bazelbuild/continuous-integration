c6cda714502872892b2cc74af24ab018e864fb8b
Damien Martin-Guillerez <dmarting@google.com>
2017-04-18 13:50:29 +0200

    Replace the gerrit-verifier job with a pipeline job
    This cleans up a bit the code of the verifier job:
      - Use the GerritUtils library
      - Create a build step "gerritReview" to wrap a build step
      - Rename to "gerrit-verifier"
    
    The "only" thing missing now for #12 is the use of gerritReview step
    to be moved to the downstream job so we can set wait:false. That
    requires the downstream job to be moved to pipeline.
    
    Change-Id: I6a58e50ae693b1cac77b993eb3897dd61c579ed2

---
+++ jenkins/gerrit-verifier.groovy
@@ -0,0 +1,66 @@
+// Copyright (C) 2017 The Bazel Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import build.bazel.ci.GerritUtils
+import build.bazel.ci.JenkinsUtils
+
+GerritUtils gerrit = new GerritUtils(
+    "https://bazel-review.googlesource.com/",
+    "/opt/secrets/gerritcookies",
+    "Bazel CI <ci.bazel@gmail.com>")
+
+// Build the jobs associated to a given change.
+def buildChange(gerrit, change) {
+  def refspec = "+" + change.ref + ":" + change.ref.replaceAll('ref/', 'ref/remotes/origin/')
+  def jobs = JenkinsUtils.jobsWithDescription(null, "Gerrit project: " + change.project + ".")
+  def changeUrl = gerrit.url(change.number, change.patchNumber)
+  if (jobs != null && !jobs.empty) {
+    gerrit.addReviewer(change.number)
+    for(job in jobs) {
+      try {
+        gerritReview(gerrit.server, "/opt/secrets/gerritcookies", gerrit.reviewer, change.number, change.sha1) {
+          r = build job: job, propagate: false, parameters: [
+            [$class: 'StringParameterValue', name: 'REFSPEC', value: refspec],
+            [$class: 'StringParameterValue', name: 'BRANCH', value: change.sha1],
+            [$class: 'StringParameterValue', name: 'CHANGE_URL', value: changeUrl]]
+          // Trick to propagate the result, the gerritReview step should live in the side of the child job.
+          gerritBuild = r
+        }
+      } catch(err) {
+        // swallow exception to avoid failing the build for a downstream failure
+      }
+    }
+  }
+}
+
+timeout(240) {
+  def changes = [:]
+  // Get open gerrit changes that were verified but not yet processed
+  stage("Get changes") {
+    def acceptedChanges = gerrit.getVerifiedChanges()
+    if (acceptedChanges) {
+      echo "Gerrit has " + acceptedChanges.size() + " change(s) to be verified"
+      acceptedChanges.each {
+        changes[it.number] = { -> buildChange(gerrit, it) }
+      }
+    } else {
+      echo "No change to be verified"
+    }
+  }
+
+  // And build accepted changes
+  stage("Verify") {
+    parallel changes
+  }
+}
---
+++ jenkins/lib/vars/gerritReview.groovy
@@ -0,0 +1,42 @@
+// Copyright (C) 2017 The Bazel Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import build.bazel.ci.GerritUtils
+
+/**
+ * Define a step "gerritReview" that wrap a build to be a "Gerrit review": the change
+ * will be commented according to the result of the build and the verified bit will be
+ * updated.
+ * except if that argument is empty (in which case the body is executed directly).
+ */
+def call(String server, String cookiesFile, String reviewer, changeNum, branch, Closure body) {
+  GerritUtils gerrit = new GerritUtils(server, cookiesFile, reviewer)
+  def url = gerrit.url(changeNum)
+  this.gerritBuild = currentBuild
+  stage("Start Gerrit review") {
+    echo "Reviewing change ${url} (${branch})"
+    gerrit.addReviewer(changeNum)
+    gerrit.comment(changeNum, branch, "Starting build at ${currentBuild.getAbsoluteUrl()}")
+  }
+  def config = [gerritBuild: currentBuild]
+  try {
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+  } finally {
+    def verified = config.gerritBuild.result == "SUCCESS" ? "+" : "-"
+    echo "Setting ${verified}Verified to change ${url} after build returned ${config.gerritBuild.result}"
+    gerrit.review(changeNum, branch, gerritBuild.result == "SUCCESS" ? 1 : -1,
+                  "Build ${config.gerritBuild.getAbsoluteUrl()} finished with status ${config.gerritBuild.result}")
+  }
+}
