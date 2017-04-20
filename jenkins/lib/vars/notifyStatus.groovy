905ad6244375069b747c594e384857e00a43ad1d
Damien Martin-Guillerez <dmarting@google.com>
2017-04-20 19:11:59 +0200

    Add a step to notify by mail about build status
    That should allow us to customize the way we do
    notification about build status in the future.
    
    Change-Id: Ia3d1caaf84754a495034950491c82b4d905f8858

---
+++ jenkins/lib/vars/notifyStatus.groovy
@@ -0,0 +1,38 @@
+// Copyright (C) 2017 The Bazel Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Define a step "notifyStatus" to notify the status of the
+ * current build. Currently only send a mail to "recipients".
+ */
+def call(String recipients, Closure body) {
+  try {
+    body()
+  } finally {
+    stage("Sending results") {
+      if (recipients != null && !recipients.isEmpty()) {
+        node("deploy") {
+          // Why do we even need a node to send a mail?
+          // TODO(dmarting): maybe use mailext?
+          step([$class: 'Mailer',
+                notifyEveryUnstableBuild: false,
+                recipients: recipients,
+                sendToIndividuals: false])
+        }
+      } else {
+        echo "Mail notifications disabled"
+      }
+    }
+  }
+}
