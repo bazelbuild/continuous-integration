3c8dddeecc3ef7e602666e8e96e9cb5cbc8567a9
Philipp Wollermann <philwo@google.com>
2018-01-24 15:45:08 +0100

    Use sudo in the mac-clean.sh script.

--- mac/mac-clean.sh
+++ mac/mac-clean.sh
@@ -5,7 +5,7 @@ set -eux
 sudo launchctl unload /Library/LaunchDaemons/jenkins.plist
 
 cd /Users/ci
-rm -rf /var/tmp/_bazel_ci workspace android-ndk-* android-sdk-* node jenkins.slave.*.log slave.jar
+sudo rm -rf /var/tmp/_bazel_ci workspace android-ndk-* android-sdk-* node jenkins.slave.*.log slave.jar
 curl https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/mac/mac-android.sh | bash
 curl https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/mac/mac-nodejs.sh | bash
 curl -o slave.jar http://master.ci.bazel.io/jnlpJars/slave.jar
