06ce80a664b94bf9c47ec017ed48b7d0f7e9515b
Jakob Buchgraber <buchgr@google.com>
2018-06-21 14:31:29 +0200

    increase number of macos jobs a bit

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -705,7 +705,7 @@ def concurrent_test_jobs(platform):
     if platform == "windows":
         return str(multiprocessing.cpu_count() // 4)
     elif platform == "macos":
-        return str(multiprocessing.cpu_count() // 4)
+        return str(multiprocessing.cpu_count() // 3)
     else:
         return str(multiprocessing.cpu_count() // 2)
 
