0716c585f91408e78c4aa0ff830fda5ed325c08d
Yun Peng <pcloudy@google.com>
2018-10-16 15:12:42 +0200

    Disable bazel-toolchains from downstream job
    bazel-toolchains need to adapt incompatible changes and fix their bug.
    See https://github.com/bazelbuild/bazel-toolchains/issues/198
    @philwo @mhlopko

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -57,10 +57,11 @@ DOWNSTREAM_PROJECTS = {
         "git_repository": "https://github.com/bazelbuild/BUILD_file_generator.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/BUILD_file_generator/master/.bazelci/presubmit.yml"
     },
-    "bazel-toolchains": {
-        "git_repository": "https://github.com/bazelbuild/bazel-toolchains.git",
-        "http_config": "https://raw.githubusercontent.com/bazelbuild/bazel-toolchains/master/.bazelci/presubmit.yml"
-    },
+    # TODO(bazel): enable once https://github.com/bazelbuild/bazel-toolchains/issues/198 is fixed
+    # "bazel-toolchains": {
+    #     "git_repository": "https://github.com/bazelbuild/bazel-toolchains.git",
+    #     "http_config": "https://raw.githubusercontent.com/bazelbuild/bazel-toolchains/master/.bazelci/presubmit.yml"
+    # },
     "bazel-skylib": {
         "git_repository": "https://github.com/bazelbuild/bazel-skylib.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/bazel-skylib/master/.bazelci/presubmit.yml"
