0c200ee32aab12894b289dbc55b626b1826f0bb8
Yun Peng <pcloudy@google.com>
2018-11-02 10:50:21 +0100

    Re-enable rules_jsonnet
    Fixes https://github.com/bazelbuild/continuous-integration/issues/369

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -148,11 +148,10 @@ DOWNSTREAM_PROJECTS = {
 #         "git_repository": "https://github.com/bazelbuild/rules_gwt.git",
 #         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_gwt/master/.bazelci/presubmit.yml"
 #     },
-     # TODO(https://github.com/bazelbuild/rules_jsonnet/issues/82): Reenable once fixed
-#     "rules_jsonnet": {
-#         "git_repository": "https://github.com/bazelbuild/rules_jsonnet.git",
-#         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_jsonnet/master/.bazelci/presubmit.yml"
-#     },
+     "rules_jsonnet": {
+         "git_repository": "https://github.com/bazelbuild/rules_jsonnet.git",
+         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_jsonnet/master/.bazelci/presubmit.yml"
+     },
     # TODO(https://github.com/google/glog/issues/376) Reenable when fixed        
 #     "rules_kotlin": {
 #         "git_repository": "https://github.com/bazelbuild/rules_kotlin.git",
