98f28b011acfae4179f91034959db0640192b58a
Jakob Buchgraber <buchgr@google.com>
2018-12-06 15:51:10 +0100

    Update bazelci.py

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -135,7 +135,7 @@ DOWNSTREAM_PROJECTS = {
     "rules_docker": {
         "git_repository": "https://github.com/bazelbuild/rules_docker.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_docker/master/.bazelci/presubmit.yml",
-        "pipeline_slug": "rules-docker-docker",
+        "pipeline_slug": "rules-docker-docker"
     },
     "rules_foreign_cc": {
         "git_repository": "https://github.com/bazelbuild/rules_foreign_cc.git",
