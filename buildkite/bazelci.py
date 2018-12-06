fad6d1ccd8e51e2812df4bbf0e1523515b25ad8f
Jakob Buchgraber <bucjac@gmail.com>
2018-12-06 16:17:06 +0100

    re-enable rules_typescript
    the blocking issue has been fixed.

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -206,8 +206,7 @@ DOWNSTREAM_PROJECTS = {
     "rules_typescript": {
         "git_repository": "https://github.com/bazelbuild/rules_typescript.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_typescript/master/.bazelci/presubmit.yml",
-        "pipeline_slug": "rules-typescript-typescript",
-        "disabled_reason": "Re-enable once fixed: https://github.com/bazelbuild/bazel/issues/6675"
+        "pipeline_slug": "rules-typescript-typescript"
     },
     "rules_webtesting": {
         "git_repository": "https://github.com/bazelbuild/rules_webtesting.git",
