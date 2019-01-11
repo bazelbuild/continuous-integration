9907ae5ae94aca67f6e27b055ac678162ce32ad7
Philipp Wollermann <philwo@google.com>
2019-01-11 11:56:25 +0100

    Push to bazel-untrusted, too
    Change-Id: I3415d2f160994b41ce8e8d022b21d7165b192733

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -5,10 +5,19 @@ set -euxo pipefail
 docker build --target ubuntu1404-java8 -t gcr.io/bazel-public/ubuntu1404:java8 .
 docker push gcr.io/bazel-public/ubuntu1404:java8
 
+docker build --target ubuntu1404-java8 -t gcr.io/bazel-untrusted/ubuntu1404:java8 .
+docker push gcr.io/bazel-untrusted/ubuntu1404:java8
+
 docker build --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
 docker push gcr.io/bazel-public/ubuntu1604:java8
+
+docker build --target ubuntu1604-java8 -t gcr.io/bazel-untrusted/ubuntu1604:java8 .
+docker push gcr.io/bazel-untrusted/ubuntu1604:java8
 
 for java in java8 java9 java10 nojava; do
   docker build --target ubuntu1804-$java -t gcr.io/bazel-public/ubuntu1804:$java .
   docker push gcr.io/bazel-public/ubuntu1804:$java
+
+  docker build --target ubuntu1804-$java -t gcr.io/bazel-untrusted/ubuntu1804:$java .
+  docker push gcr.io/bazel-untrusted/ubuntu1804:$java
 done
