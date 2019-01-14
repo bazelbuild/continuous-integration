bc7ae6eb84173335cb5fe0df877354c9fb1b05a3
Philipp Wollermann <philwo@google.com>
2019-01-14 16:45:16 +0100

    Add Docker container for ubuntu1804:java11
    Change-Id: I43bdf74a86b90dee4e4bcf9597a44862c5b6f3a8

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -14,7 +14,7 @@ docker push gcr.io/bazel-public/ubuntu1604:java8
 docker build --target ubuntu1604-java8 -t gcr.io/bazel-untrusted/ubuntu1604:java8 .
 docker push gcr.io/bazel-untrusted/ubuntu1604:java8
 
-for java in java8 java9 java10 nojava; do
+for java in java8 java9 java10 java11 nojava; do
   docker build --target ubuntu1804-$java -t gcr.io/bazel-public/ubuntu1804:$java .
   docker push gcr.io/bazel-public/ubuntu1804:$java
 
