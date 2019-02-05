ee85078eaefcd8a2bd3f400296a1c27bca9c234d
Philipp Wollermann <philwo@google.com>
2019-02-05 22:56:04 +0100

    Fix name of Cloud Robotics Core pipeline

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -118,7 +118,7 @@ DOWNSTREAM_PROJECTS = {
         "http_config": "https://raw.githubusercontent.com/bazelbuild/intellij/master/.bazelci/clion.yml",
         "pipeline_slug": "clion-plugin",
     },
-    "cloud-robotics core": {
+    "Cloud Robotics Core": {
         "git_repository": "https://github.com/googlecloudrobotics/core.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/pipelines/cloud-robotics-postsubmit.yml",
         "pipeline_slug": "cloud-robotics-core",
