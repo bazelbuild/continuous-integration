93fb8302dad43e34ebe35edc4d5da0bd2698dc98
Marcel Hlopko <hlopko@bazel.build>
2019-02-25 08:31:33 +0100

    Only forward LocalAppData when it is defined
    Let's see if this works.
    
    https://github.com/bazelbuild/continuous-integration/commit/91d848aefb1696c4913b3f9231916edf7b12a18e added forwarding LocalAppData variable to bazelisk, but it appears it's not defined in our workers. Let's only add it when it exists. https://buildkite.com/bazel/bazel-bazel/builds/6915#3ba9964c-3edd-44a1-9267-901114ff80f8

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -550,7 +550,10 @@ def execute_commands(
     try:
         # If the CI worker runs Bazelisk, we need to forward all required env variables to the test.
         # Otherwise any integration test that invokes Bazel (=Bazelisk in this case) will fail.
-        test_env_vars = ["LocalAppData"] if platform == "windows" else ["HOME"]
+        if platform == "windows" and "LocalAppData" in os.environ:
+          test_env_vars = ["LocalAppData"]
+        else:
+          test_env_vars = ["HOME"]
         if git_repo_location:
             os.chdir(git_repo_location)
         elif git_repository:
