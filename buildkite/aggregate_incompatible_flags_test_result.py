8296116f64906fbd465df8d78959c0f9f050c48c
Yun Peng <pcloudy@google.com>
2019-03-13 10:58:11 +0100

    Bazelisk inc pipeline:  Print projects that need migration (#556)
    * Run python formatter
    
    Change-Id: I71632b8cfc6d9c52ec9ab2d20d2d673b20d43187
    
    * Print projects that need migration
    
    Change-Id: Iccd454a31217a2c30c81ec84b2ddaa9d1c9bf02b
    
    * Address comments
    
    Change-Id: Idb4bd4bbdb1e45a5368ec4825983a68e2953ca28
    
    * Also print projects in one line for projects need migration
    
    * Display project number instead of job num
    
    Change-Id: I5927b76df7e47a2a44e7cab105fd2f95c00c6c0a

--- buildkite/aggregate_incompatible_flags_test_result.py
+++ buildkite/aggregate_incompatible_flags_test_result.py
@@ -29,7 +29,6 @@ PIPELINE = "bazelisk-plus-incompatible-flags"
 
 
 class LogFetcher(threading.Thread):
-
     def __init__(self, job, client):
         threading.Thread.__init__(self)
         self.job = job
@@ -56,7 +55,7 @@ def process_build_log(failed_jobs_per_flag, already_failing_jobs, log, job):
             line = line.strip()
             if line.startswith("--incompatible_") and line in INCOMPATIBLE_FLAGS:
                 failed_jobs_per_flag[line][job["id"]] = job
-        log = log[0: log.rfind("+++ Result")]
+        log = log[0 : log.rfind("+++ Result")]
 
     # If the job failed for other reasons, we add it into already failing jobs.
     if job["state"] == "failed":
@@ -64,54 +63,95 @@ def process_build_log(failed_jobs_per_flag, already_failing_jobs, log, job):
 
 
 def get_html_link_text(content, link):
-    return f"<a href=\"{link}\" target=\"_blank\">{content}</a>"
+    return f'<a href="{link}" target="_blank">{content}</a>'
 
 
-def construct_success_info(failed_jobs_per_flag):
+def print_flags_ready_to_flip(failed_jobs_per_flag):
     info_text = ["#### The following flags didn't break any passing jobs"]
     for flag in INCOMPATIBLE_FLAGS:
         if flag not in failed_jobs_per_flag:
             github_url = INCOMPATIBLE_FLAGS[flag]
             info_text.append(f"* **{flag}** " + get_html_link_text(":github:", github_url))
     if len(info_text) == 1:
-        return None
-    return info_text
+        return
+    print_info("flags_ready_to_flip", "success", info_text)
 
 
-def construct_warning_info(already_failing_jobs):
+def print_already_fail_jobs(already_failing_jobs):
     info_text = ["#### The following jobs already fail without incompatible flags"]
-    info_text += merge_and_format_jobs(already_failing_jobs, "* ")
+    info_text += merge_and_format_jobs(already_failing_jobs, "* **{}**: {}")
     if len(info_text) == 1:
-        return None
-    return info_text
+        return
+    print_info("already_fail_jobs", "warning", info_text)
 
 
-def construct_failure_info(failed_jobs_per_flag):
+def print_projects_need_to_migrate(failed_jobs_per_flag):
+    info_text = ["#### The following projects need migration"]
+    jobs_need_migration = {}
+    for jobs in failed_jobs_per_flag.values():
+        for job in jobs.values():
+            jobs_need_migration[job["name"]] = job
+
+    job_list = [i[1] for i in sorted(jobs_need_migration.items(), key=lambda s: s[0].lower())]
+    job_num = len(job_list)
+    if job_num == 0:
+        return
+
+    projects = set()
+    for job in job_list:
+        project, _ = get_pipeline_and_platform(job)
+        projects.add(project)
+    project_num = len(projects)
+
+    s1 = "" if project_num == 1 else "s"
+    s2 = "s" if project_num == 1 else ""
+    info_text.append(
+        f"<details><summary>{project_num} project{s1} need{s2} migration, click to see details</summary><ul>"
+    )
+
+    entries = merge_and_format_jobs(job_list, "    <li><strong>{}</strong>: {}</li>")
+    info_text += entries
+    info_text.append("</ul></details>")
+
+    info_str = "\n".join(info_text)
+    bazelci.execute_command(
+        [
+            "buildkite-agent",
+            "annotate",
+            "--append",
+            f"--context=projects_need_migration",
+            f"--style=warning",
+            f"\n{info_str}\n",
+        ]
+    )
+
+
+def print_flags_need_to_migrate(failed_jobs_per_flag):
     info_text = ["#### Downstream projects need to migrate for the following flags"]
     for flag, jobs in failed_jobs_per_flag.items():
         if jobs:
             github_url = INCOMPATIBLE_FLAGS[flag]
             info_text.append(f"* **{flag}** " + get_html_link_text(":github:", github_url))
-            info_text += merge_and_format_jobs(jobs.values(), "  - ")
+            info_text += merge_and_format_jobs(jobs.values(), "  - **{}**: {}")
     if len(info_text) == 1:
-        return None
-    return info_text
+        return
+    print_info("flags_need_to_migrate", "error", info_text)
 
 
-def merge_and_format_jobs(jobs, line_prefix):
+def merge_and_format_jobs(jobs, line_pattern):
     # Merges all jobs for a single pipeline into one line.
     # Example:
     #   pipeline (platform1)
     #   pipeline (platform2)
     #   pipeline (platform3)
-    # with line_prefix ">> " becomes
+    # with line_pattern ">> {}: {}" becomes
     #   >> pipeline: platform1, platform2, platform3
     jobs_per_pipeline = collections.defaultdict(list)
     for job in jobs:
         pipeline, platform = get_pipeline_and_platform(job)
         jobs_per_pipeline[pipeline].append(get_html_link_text(platform, job["web_url"]))
 
-    return ["{}**{}**: {}".format(line_prefix, pipeline, ", ".join(platforms)) for pipeline, platforms in jobs_per_pipeline.items()]
+    return [line_pattern.format(pipeline, ", ".join(platforms)) for pipeline, platforms in jobs_per_pipeline.items()]
 
 
 def get_pipeline_and_platform(job):
@@ -124,8 +164,17 @@ def print_info(context, style, info):
     # CHUNK_SIZE is to prevent buildkite-agent "argument list too long" error
     CHUNK_SIZE = 20
     for i in range(0, len(info), CHUNK_SIZE):
-        info_str = "\n".join(info[i:i + CHUNK_SIZE])
-        bazelci.execute_command(["buildkite-agent", "annotate", "--append", f"--context={context}", f"--style={style}", f"\n{info_str}\n"])
+        info_str = "\n".join(info[i : i + CHUNK_SIZE])
+        bazelci.execute_command(
+            [
+                "buildkite-agent",
+                "annotate",
+                "--append",
+                f"--context={context}",
+                f"--style={style}",
+                f"\n{info_str}\n",
+            ]
+        )
 
 
 def print_result_info(build_number, client):
@@ -148,20 +197,13 @@ def print_result_info(build_number, client):
         thread.join()
         process_build_log(failed_jobs_per_flag, already_failing_jobs, thread.log, thread.job)
 
-    failure_info = construct_failure_info(failed_jobs_per_flag)
+    print_flags_need_to_migrate(failed_jobs_per_flag)
 
-    warning_info = construct_warning_info(already_failing_jobs)
+    print_projects_need_to_migrate(failed_jobs_per_flag)
 
-    success_info = construct_success_info(failed_jobs_per_flag)
+    print_already_fail_jobs(already_failing_jobs)
 
-    if failure_info:
-        print_info("failure", "error", failure_info)
-
-    if warning_info:
-        print_info("warning", "warning", warning_info)
-
-    if success_info:
-        print_info("success", "success", success_info)
+    print_flags_ready_to_flip(failed_jobs_per_flag)
 
 
 def main(argv=None):
