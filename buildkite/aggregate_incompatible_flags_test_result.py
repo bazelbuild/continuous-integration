464ca28ff9288f614d6e6cc4a0b6d102d99d9538
Yun Peng <pcloudy@google.com>
2019-03-27 10:16:24 +0100

    Fix the order
    Change-Id: I4e4e6e1ae33df27df0046ebb9999c26f4ee9865e

--- buildkite/aggregate_incompatible_flags_test_result.py
+++ buildkite/aggregate_incompatible_flags_test_result.py
@@ -127,10 +127,7 @@ def print_projects_need_to_migrate(failed_jobs_per_flag):
 
 
 def print_flags_need_to_migrate(failed_jobs_per_flag):
-    info_text = ["#### Downstream projects need to migrate for the following flags"]
-    if failed_jobs_per_flag:
-        print_info("flags_need_to_migrate", "error", info_text)
-    for flag, jobs in failed_jobs_per_flag.items():
+    for flag, jobs in reversed(failed_jobs_per_flag.items()):
         if jobs:
             github_url = INCOMPATIBLE_FLAGS[flag]
             info_text = []
@@ -139,6 +136,8 @@ def print_flags_need_to_migrate(failed_jobs_per_flag):
             print_info(flag, "error", info_text)
     if len(info_text) == 1:
         return
+    info_text = ["#### Downstream projects need to migrate for the following flags"]
+    print_info("flags_need_to_migrate", "error", info_text)
 
 
 def merge_and_format_jobs(jobs, line_pattern):
