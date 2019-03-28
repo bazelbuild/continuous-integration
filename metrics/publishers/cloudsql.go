1bb6133946debf7163a89232fc8b0baa086f8171
Florian Weikert <fwe@google.com>
2019-03-28 15:00:48 +0100

    Rename Cloud SQL publisher.

--- metrics/publishers/cloudsql.go
+++ metrics/publishers/cloudsql.go
@@ -11,6 +11,11 @@ type CloudSql struct {
 
 func (c CloudSql) Publish(metricName string, newData *data.DataSet) error {
 	fmt.Printf("Got %v from %s\n", newData, metricName)
+	// 1. Lock table
+	// 2. insert row, ignore
+	// http://bogdan.org.ua/2007/10/18/mysql-insert-if-not-exists-syntax.html
+	// https://dev.mysql.com/doc/refman/8.0/en/insert-on-duplicate.html
+	// https://stackoverflow.com/questions/3164505/mysql-insert-record-if-not-exists-in-table
 	return nil
 }
 
