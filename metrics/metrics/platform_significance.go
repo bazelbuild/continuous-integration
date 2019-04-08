8cb58887888527f219016ebb634a466fe6b33565
Florian Weikert <fwe@google.com>
2019-04-08 15:18:02 +0200

    Add pipeline_significance metric.

---
+++ metrics/metrics/platform_significance.go
@@ -0,0 +1,131 @@
+package metrics
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fweikert/continuous-integration/metrics/clients"
+	"github.com/fweikert/continuous-integration/metrics/data"
+)
+
+type PlatformSignificance struct {
+	buildSuccess *BuildSuccess
+	columns      []Column
+}
+
+func (ps *PlatformSignificance) Name() string {
+	return "platform_significance"
+}
+
+func (ps *PlatformSignificance) Columns() []Column {
+	return ps.columns
+}
+
+type pipelineStats struct {
+	totalBuilds           int
+	passingBuilds         int
+	canceledBuilds        int
+	linuxFailures         int
+	macosFailures         int
+	windowsFailures       int
+	rbeFailures           int
+	multiPlatformFailures int
+}
+
+func (ps *PlatformSignificance) Collect() (*data.DataSet, error) {
+	buildResult, err := ps.buildSuccess.Collect()
+	if err != nil {
+		return nil, err
+	}
+
+	stats, err := collectPipelineResults(buildResult)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to collect pipeline results: %v", err)
+	}
+
+	result := data.CreateDataSet(GetColumnNames(ps.columns))
+	for pipeline, values := range stats {
+		result.AddRow(pipeline, values.totalBuilds, values.passingBuilds, values.canceledBuilds, values.linuxFailures, values.macosFailures, values.windowsFailures, values.rbeFailures, values.multiPlatformFailures)
+	}
+	return result, nil
+}
+
+func collectPipelineResults(buildResult *data.DataSet) (map[string]*pipelineStats, error) {
+	stats := make(map[string]*pipelineStats)
+	for _, row := range buildResult.Data {
+		values, err := toString(row)
+		if err != nil {
+			return nil, fmt.Errorf("Could not process build_success results: %v", err)
+		}
+
+		pipeline := values[0]
+		if _, ok := stats[pipeline]; !ok {
+			stats[pipeline] = &pipelineStats{}
+		}
+
+		passed := true
+		canceled := false
+		failures := make([]int, 0)
+		for i := 1; i < len(values); i += 1 {
+			if values[i] != passingState {
+				passed = false
+				if values[i] == canceledState {
+					canceled = true
+					break
+				} else if values[i] == failedState {
+					failures = append(failures, i)
+				}
+			}
+		}
+
+		stats[pipeline].totalBuilds += 1
+		if canceled {
+			stats[pipeline].canceledBuilds += 1
+		} else if passed {
+			stats[pipeline].passingBuilds += 1
+		} else if len(failures) > 1 {
+			stats[pipeline].multiPlatformFailures += 1
+		} else if len(failures) == 1 {
+			switch failures[0] {
+			case 1:
+				stats[pipeline].linuxFailures += 1
+				log.Printf("Linux only: %s/%d\n", pipeline, row[1])
+			case 2:
+				stats[pipeline].macosFailures += 1
+				log.Printf("MacOS only: %s/%d\n", pipeline, row[1])
+			case 3:
+				stats[pipeline].windowsFailures += 1
+				log.Printf("Windows only: %s/%d\n", pipeline, row[1])
+			case 4:
+				stats[pipeline].rbeFailures += 1
+				log.Printf("RBE only: %s/%d\n", pipeline, row[1])
+			}
+		}
+	}
+	return stats, nil
+}
+
+func toString(row []interface{}) ([]string, error) {
+	result := make([]string, len(row)-1)
+	to := 0
+	for i, v := range row {
+		if i == 1 {
+			// Skip build numnber.
+			continue
+		}
+		str, ok := v.(string)
+		if !ok {
+			return nil, fmt.Errorf("Expected string in column %v: %s", i, v)
+		}
+		result[to] = str
+		to += 1
+	}
+	return result, nil
+}
+
+// CREATE TABLE platform_significance (pipeline VARCHAR(255), total_builds INT, passing_builds INT, canceled_builds INT, linux_failures INT, macos_failures INT, windows_failures INT, rbe_failures INT, multi_platform_failures INT, PRIMARY KEY(pipeline));
+func CreatePlatformSignificance(client *clients.BuildkiteClient, builds int, pipelines ...string) *PlatformSignificance {
+	buildSuccess := CreateBuildSuccess(client, builds, pipelines...)
+	columns := []Column{Column{"pipeline", true}, Column{"total_builds", false}, Column{"passing_builds", false}, Column{"canceled_builds", false}, Column{"linux_failures", false}, Column{"macos_failures", false}, Column{"windows_failures", false}, Column{"rbe_failures", false}, Column{"multi_platform_failures", false}}
+	return &PlatformSignificance{buildSuccess: buildSuccess, columns: columns}
+}
