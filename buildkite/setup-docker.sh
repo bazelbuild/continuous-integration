7c8bae4f249f57834f95fe5b7d7fb4e93eaf3bc6
Philipp Wollermann <philwo@google.com>
2019-04-15 17:44:39 +0200

    Run "docker system prune -f --volumes" regularly

--- buildkite/setup-docker.sh
+++ buildkite/setup-docker.sh
@@ -57,18 +57,9 @@ EOF
 [Service]
 Restart=always
 PermissionsStartOnly=true
-# Disable tasks accounting, because Bazel is prone to run into resource limits there.
-# This fixes the "cgroup: fork rejected by pids controller" error that some CI jobs triggered.
-TasksAccounting=no
-EOF
-
-  mkdir /etc/systemd/system/buildkite-agent@.service.d
-  cat > /etc/systemd/system/buildkite-agent@.service.d/override.conf <<'EOF'
-[Service]
-Restart=always
-PermissionsStartOnly=true
-Environment=BUILDKITE_AGENT_NAME=%%hostname-%i
-Environment=BUILDKITE_AGENT_PRIORITY=%i
+ExecStartPre=/usr/bin/docker container prune -f
+ExecStartPre=/bin/bash -c 'x="$(docker ps -a -q)"; test "$x" && docker kill $x'
+ExecStartPre=/usr/bin/docker system prune -f --volumes
 # Disable tasks accounting, because Bazel is prone to run into resource limits there.
 # This fixes the "cgroup: fork rejected by pids controller" error that some CI jobs triggered.
 TasksAccounting=no
