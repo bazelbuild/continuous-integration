532e7d9d0e6be3e364fbf38d7e429e910cd2c5a2
Florian Weikert <fwe@google.com>
2019-05-09 12:47:21 +0200

    Add Csv format for Stdout publisher.
    This commit also fixes a previous bug where any given formatter wasn't used at all.

--- metrics/data/data.go
+++ metrics/data/data.go
@@ -12,7 +12,7 @@ type DataSet struct {
 
 func (data *DataSet) AddRow(values ...interface{}) error {
 	if len(values) != len(data.Headers) {
-		return fmt.Errorf("DataSet has %d columns (%s), but new row has %d (values: %s).", len(data.Headers), strings.Join(data.Headers, ", "), len(values), strings.Join(getValuesAsStrings(values), ", "))
+		return fmt.Errorf("DataSet has %d columns (%s), but new row has %d (values: %s).", len(data.Headers), strings.Join(data.Headers, ", "), len(values), strings.Join(GetRowAsStrings(values), ", "))
 	}
 	data.Data = append(data.Data, values)
 	return nil
@@ -22,14 +22,14 @@ func (data *DataSet) String() string {
 	lines := make([]string, len(data.Data)+1)
 	lines[0] = strings.Join(data.Headers, "\t")
 	for i, row := range data.Data {
-		lines[i+1] = strings.Join(getValuesAsStrings(row), "\t")
+		lines[i+1] = strings.Join(GetRowAsStrings(row), "\t")
 	}
 	return strings.Join(lines, "\n")
 }
 
-func getValuesAsStrings(values []interface{}) []string {
-	stringValues := make([]string, len(values))
-	for i, v := range values {
+func GetRowAsStrings(row []interface{}) []string {
+	stringValues := make([]string, len(row))
+	for i, v := range row {
 		stringValues[i] = fmt.Sprintf("%v", v)
 	}
 	return stringValues
--- metrics/publishers/stdout.go
+++ metrics/publishers/stdout.go
@@ -2,12 +2,28 @@ package publishers
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fweikert/continuous-integration/metrics/data"
 	"github.com/fweikert/continuous-integration/metrics/metrics"
 )
 
+type formatter func(*data.DataSet) string
+
+func PlainText(newData *data.DataSet) string {
+	return newData.String()
+}
+
+func Csv(newData *data.DataSet) string {
+	lines := make([]string, len(newData.Data))
+	for i, row := range newData.Data {
+		lines[i] = strings.Join(data.GetRowAsStrings(row), ";")
+	}
+	return strings.Join(lines, "\n")
+}
+
 type Stdout struct {
+	formatFunc formatter
 }
 
 func (stdout *Stdout) Name() string {
@@ -20,6 +36,10 @@ func (stdout *Stdout) RegisterMetric(metric metrics.Metric) error {
 }
 
 func (stdout *Stdout) Publish(metricName string, newData *data.DataSet) error {
-	fmt.Printf("Metric %s:\n%s\n", metricName, newData)
+	fmt.Printf("Metric %s:\n%s\n", metricName, stdout.formatFunc(newData))
 	return nil
+}
+
+func CreateStdoutPublisher(formatFunc formatter) *Stdout {
+	return &Stdout{formatFunc: formatFunc}
 }
