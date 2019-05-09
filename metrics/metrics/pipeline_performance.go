f93aea98b8d6c3fedc1274b02c9b6e1b0459287d
Florian Weikert <fwe@google.com>
2019-05-09 15:08:34 +0200

    Allow pipeline performance metric to filter by platform.

--- metrics/metrics/pipeline_performance.go
+++ metrics/metrics/pipeline_performance.go
@@ -10,10 +10,11 @@ import (
 const skipTasksEnvVar = "CI_SKIP_TASKS"
 
 type PipelinePerformance struct {
-	client      *clients.BuildkiteClient
-	pipelines   []string
-	columns     []Column
-	lastNBuilds int
+	client         *clients.BuildkiteClient
+	pipelines      []string
+	columns        []Column
+	lastNBuilds    int
+	platformFilter string
 }
 
 func (pp *PipelinePerformance) Name() string {
@@ -34,6 +35,9 @@ func (pp *PipelinePerformance) Collect() (*data.DataSet, error) {
 		for _, build := range builds {
 			skippedTasks := build.Env[skipTasksEnvVar]
 			for _, job := range build.Jobs {
+				if pp.platformFilter != "" && getPlatfrom(job) != pp.platformFilter {
+					continue
+				}
 				err := result.AddRow(pipeline, *build.Number, *job.Name, job.CreatedAt, getDifferenceSeconds(job.CreatedAt, job.StartedAt), getDifferenceSeconds(job.StartedAt, job.FinishedAt), skippedTasks)
 				if err != nil {
 					return nil, fmt.Errorf("Failed to add result for job %s of build %d: %v", *job.Name, *build.Number, err)
@@ -45,7 +49,7 @@ func (pp *PipelinePerformance) Collect() (*data.DataSet, error) {
 }
 
 // CREATE TABLE pipeline_performance (pipeline VARCHAR(255), build INT, job VARCHAR(255), creation_time DATETIME, wait_time_seconds FLOAT, run_time_seconds FLOAT, skipped_tasks VARCHAR(255), PRIMARY KEY(pipeline, build, job));
-func CreatePipelinePerformance(client *clients.BuildkiteClient, lastNBuilds int, pipelines ...string) *PipelinePerformance {
+func CreatePipelinePerformance(client *clients.BuildkiteClient, lastNBuilds int, platformFilter string, pipelines ...string) *PipelinePerformance {
 	columns := []Column{Column{"pipeline", true}, Column{"build", true}, Column{"job", true}, Column{"creation_time", false}, Column{"wait_time_seconds", false}, Column{"run_time_seconds", false}, Column{"skipped_tasks", false}}
-	return &PipelinePerformance{client: client, pipelines: pipelines, columns: columns, lastNBuilds: lastNBuilds}
+	return &PipelinePerformance{client: client, pipelines: pipelines, columns: columns, lastNBuilds: lastNBuilds, platformFilter: platformFilter}
 }
