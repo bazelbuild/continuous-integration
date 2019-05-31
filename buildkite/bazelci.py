458d187dfd3efbc2c078164609d44e4c4178a97b
Florian Weikert <fwe@google.com>
2019-05-31 17:22:52 +0200

    Properly handle tasks when importing configs

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -650,7 +650,7 @@ def load_imported_tasks(import_name, http_url, file_config):
 
     namespace = import_name.partition(".")[0]
     tasks = {}
-    for task_name, task_config in imported_config.items():
+    for task_name, task_config in imported_config["tasks"].items():
         if "platform" not in task_config:
             task_config["platform"] = task_name
 
