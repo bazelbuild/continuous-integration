e188426c47fef54d0024deca4c5d2154350377e2
Arielle Albon <ariellea@google.com>
2019-06-13 11:58:30 +0200

    Small change to turn on Buildkit builder for all docker images. (#722)

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+#Enable use of buildkit for all builds. No extra support in the docker file is required.
+export DOCKER_BUILDKIT=1
+
+
 docker build --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
 docker build --target ubuntu1804-java11 -t gcr.io/bazel-public/ubuntu1804:java11 .
 docker build --target ubuntu1804-nojava -t gcr.io/bazel-public/ubuntu1804:nojava .
