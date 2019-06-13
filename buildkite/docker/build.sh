e05485d030d984bf81057cc290ab33041cc5e3e1
Philipp Wollermann <philwo@google.com>
2019-06-13 14:53:19 +0200

    Update docker/build.sh and Dockerfile

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-#Enable use of buildkit for all builds. No extra support in the docker file is required.
+# Enable use of buildkit for all builds. No extra support in the Dockerfile is required.
+# See https://docs.docker.com/develop/develop-images/build_enhancements/ for details.
 export DOCKER_BUILDKIT=1
-
 
 docker build --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
 docker build --target ubuntu1804-java11 -t gcr.io/bazel-public/ubuntu1804:java11 .
