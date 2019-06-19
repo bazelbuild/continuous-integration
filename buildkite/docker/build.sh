635ad650d48290ae20c24bd55e9822dc469b6419
Arielle Albon <ariellea@google.com>
2019-06-19 07:55:35 +0200

    Initial commit for debian docker images based off the Ubuntu ones. (#719)
    These images use Buildkit instead of the standard docker build command.
    Therefor a modern version of docker is required.
    
    These images are for x86_64 at present, there is further work to complete before they can be used with aarch64 or armhf.

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -9,3 +9,6 @@ export DOCKER_BUILDKIT=1
 docker build --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
 docker build --target ubuntu1804-java11 -t gcr.io/bazel-public/ubuntu1804:java11 .
 docker build --target ubuntu1804-nojava -t gcr.io/bazel-public/ubuntu1804:nojava .
+# ariellea added configs for debian
+DOCKER_BUILDKIT=1 docker build --target debianunstable-java11 -t gcr.io/bazel-public/debian/unstable:java11 -f debian.Dockerfile .
+DOCKER_BUILDKIT=1 docker build --target debianunstable-nojava -t gcr.io/bazel-public/debian/unstable:nojava -f debian.Dockerfile .
