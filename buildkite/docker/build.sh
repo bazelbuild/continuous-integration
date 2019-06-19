10be1cbd28033950538e0a4a2b409507a42b9b43
Philipp Wollermann <philwo@google.com>
2019-06-19 10:29:12 +0200

    Refactor our Dockerfiles

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -6,9 +6,11 @@ set -euxo pipefail
 # See https://docs.docker.com/develop/develop-images/build_enhancements/ for details.
 export DOCKER_BUILDKIT=1
 
-docker build --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
-docker build --target ubuntu1804-java11 -t gcr.io/bazel-public/ubuntu1804:java11 .
-docker build --target ubuntu1804-nojava -t gcr.io/bazel-public/ubuntu1804:nojava .
-# ariellea added configs for debian
-DOCKER_BUILDKIT=1 docker build --target debianunstable-java11 -t gcr.io/bazel-public/debian/unstable:java11 -f debian.Dockerfile .
-DOCKER_BUILDKIT=1 docker build --target debianunstable-nojava -t gcr.io/bazel-public/debian/unstable:nojava -f debian.Dockerfile .
+for target in bazelisk buildifier github-release saucelabs; do
+    docker build -f base/Dockerfile --target $target -t gcr.io/bazel-public/base:$target .
+done
+
+docker build -f ubuntu1604/Dockerfile --target ubuntu1604-java8 -t gcr.io/bazel-public/ubuntu1604:java8 .
+docker build -f ubuntu1804/Dockerfile --target ubuntu1804-java11 -t gcr.io/bazel-public/ubuntu1804:java11 .
+docker build -f ubuntu1804/Dockerfile --target ubuntu1804-nojava -t gcr.io/bazel-public/ubuntu1804:nojava .
+docker build -f debian-unstable/Dockerfile --target debian-unstable-java11 -t gcr.io/bazel-public/debian-unstable:java11 .
--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -5,3 +5,4 @@ set -euxo pipefail
 docker push gcr.io/bazel-public/ubuntu1604:java8
 docker push gcr.io/bazel-public/ubuntu1804:java11
 docker push gcr.io/bazel-public/ubuntu1804:nojava
+docker push gcr.io/bazel-public/debian-unstable:java11
