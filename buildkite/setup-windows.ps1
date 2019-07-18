baac80c3ee67cf27c3cc778e55c0547b7bfb4ebe
Yun Peng <pcloudy@google.com>
2019-07-18 13:35:45 +0200

    Install LLVM(Clang) on Windows machines (#763)
    * Install LLVM(Clang) on Windows machines
    
    * Set BAZEL_LLVM
    
    * Set BAZEL_LLVM correctly

--- buildkite/setup-windows.ps1
+++ buildkite/setup-windows.ps1
@@ -68,6 +68,13 @@ Write-Host "Installing curl..."
 # FYI: choco installs curl.exe in C:\ProgramData\chocolatey\bin (which is on the PATH).
 & choco install curl
 
+## Install LLVM/Clang
+Write-Host "Installing llvm..."
+# FYI: choco installs clang in C:\Program Files\LLVM\bin (which is not on the PATH).
+& choco install llvm
+[Environment]::SetEnvironmentVariable("BAZEL_LLVM", "C:\Program Files\LLVM", "Machine")
+$env:BAZEL_LLVM = [Environment]::GetEnvironmentVariable("BAZEL_LLVM", "Machine")
+
 ## Install Git for Windows.
 Write-Host "Installing Git for Windows..."
 # FYI: choco adds "C:\Program Files\Git\cmd" to global PATH.
