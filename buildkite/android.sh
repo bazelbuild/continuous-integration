2f9b0b589622b21ab8a4bda4eb2060f685eebeca
Jin <jin@users.noreply.github.com>
2019-10-23 11:24:22 -0400

    Update Android build-tools to 29.0.2 (#856)

--- buildkite/android.sh
+++ buildkite/android.sh
@@ -20,8 +20,7 @@ rm android-sdk.zip
 yes | tools/bin/sdkmanager --licenses > /dev/null
 tools/bin/sdkmanager --update
 tools/bin/sdkmanager \
-    "build-tools;27.0.3" \
-    "build-tools;28.0.2" \
+    "build-tools;29.0.2" \
     "emulator" \
     "extras;android;m2repository" \
     "platform-tools" \
