543dd19f66fcfe7ac5bc2d14584b3f8c927ec9e8
Joe Le-Ba <leba@google.com>
2020-01-27 15:27:21 +0100

    bazel option, not bb

--- buildkite/bazel-bench/bazel_bench.py
+++ buildkite/bazel-bench/bazel_bench.py
@@ -47,10 +47,10 @@ PROJECTS = [
         "name": "TensorFlow",
         "storage_subdir": "tensorflow",
         "git_repository": "https://github.com/tensorflow/tensorflow.git",
-        "bazel_command": "build //tensorflow/tools/pip_package:build_pip_package",
+        "bazel_command": "build --output_filter=^\$ //tensorflow/tools/pip_package:build_pip_package",
         "bazel_bench_extra_options": {
-            "ubuntu1804": "--output_filter=^\$ --env_configure=\"yes '' | ./configure\"",
-            "macos": "--output_filter=^\$ --env_configure=\"yes '' | python3 ./configure.py\"",
+            "ubuntu1804": "--env_configure=\"yes '' | ./configure\"",
+            "macos": "--env_configure=\"yes '' | python3 ./configure.py\"",
         },
         # "bazel_bench_extra_options": "https://raw.githubusercontent.com/joeleba/continuous-integration/tf/buildkite/pipelines/tensorflow-bazel-bench.yml",
         "active": True,
