cb22c539add139bcad0be16bdd824fe9eb750bfd
Yun Peng <pcloudy@google.com>
2020-06-02 10:17:09 +0200

    Disable Envoy in Downstream
    It has been red for two weeks due to breakage from Envoy side:
    https://github.com/envoyproxy/envoy/issues/11400

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -175,6 +175,7 @@ DOWNSTREAM_PROJECTS_PRODUCTION = {
         "git_repository": "https://github.com/envoyproxy/envoy.git",
         "http_config": "https://raw.githubusercontent.com/envoyproxy/envoy/master/.bazelci/presubmit.yml",
         "pipeline_slug": "envoy",
+        "disabled_reason": "https://github.com/envoyproxy/envoy/issues/11400"
     },
     "FlatBuffers": {
         "git_repository": "https://github.com/google/flatbuffers.git",
