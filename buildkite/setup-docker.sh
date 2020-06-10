37183ccb4fa47a8ed79308d77142e2948ffc1da3
Philipp Wollermann <philwo@google.com>
2020-06-10 13:04:25 +0200

    Update setup-docker.sh

--- buildkite/setup-docker.sh
+++ buildkite/setup-docker.sh
@@ -205,6 +205,8 @@ EOF
   yes | tools/bin/sdkmanager --licenses > /dev/null || true
   tools/bin/sdkmanager --update
   tools/bin/sdkmanager \
+      "build-tools;28.0.2" \
+      "build-tools;29.0.3" \
       "build-tools;30.0.0-rc4" \
       "emulator" \
       "extras;android;m2repository" \
