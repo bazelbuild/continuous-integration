cf793b4e33679feab8c73768c86c8cbbe75343de
Chi Wang <coeuvre@gmail.com>
2020-12-09 23:39:40 +0800

    Added a basic table for tracking team issues

--- dashboard/client/pages/index.tsx
+++ dashboard/client/pages/index.tsx
@@ -1,3 +1,12 @@
+import styles from "./index.module.scss";
+
+import React from "react";
+import GithubTeamIssueTable from "./GithubTeamIssueTable";
+
 export default function Home() {
-  return <div />;
+  return (
+    <div className={styles.container}>
+      <GithubTeamIssueTable />
+    </div>
+  );
 }
