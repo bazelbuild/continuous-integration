4c23168b4debc557151bd54f20653b0d191dd214
Chi Wang <coeuvre@gmail.com>
2020-12-12 13:33:01 +0800

    Use trigger instead of materialized view to generate github_issue

--- dashboard/server/database.sql
+++ dashboard/server/database.sql
@@ -1,3 +1,6 @@
+--
+-- Table that store the raw issues data fetched from Github
+--
 CREATE TABLE github_issue_data
 (
     owner        TEXT        NOT NULL,
@@ -9,35 +12,104 @@ CREATE TABLE github_issue_data
     PRIMARY KEY (owner, repo, issue_number)
 );
 
-CREATE INDEX ON github_issue_data ((data -> 'pull_request'));
-
-
-CREATE MATERIALIZED VIEW github_issue AS
-WITH github_issue_label AS (
-    SELECT owner, repo, issue_number, jsonb_array_elements(data -> 'labels') ->> 'name' AS label
-    FROM github_issue_data
-),
-     github_issue_labels AS (
-         SELECT owner, repo, issue_number, array_agg(label) AS labels
-         FROM github_issue_label
-         GROUP BY owner, repo, issue_number
-     )
-SELECT i.owner,
-       i.repo,
-       i.issue_number,
-       i.data ->> 'state'                   AS state,
-       i.data ->> 'title'                   AS title,
-       COALESCE(il.labels, '{}')            AS labels,
-       (data ->> 'created_at')::timestamptz AS created_at,
-       (data ->> 'closed_at')::timestamptz  AS closed_at
-FROM github_issue_data i
-         LEFT JOIN github_issue_labels il
-                   ON i.owner = il.owner AND i.repo = il.repo AND i.issue_number = il.issue_number
-WHERE i.data -> 'pull_request' IS NULL;
+--
+-- Table that stored issues generated from github_issue_data automatically by trigger
+--
+CREATE TABLE github_issue
+(
+    owner           TEXT        NOT NULL,
+    repo            TEXT        NOT NULL,
+    issue_number    INTEGER     NOT NULL,
+    timestamp       TIMESTAMPTZ NOT NULL,
+    state           TEXT        NOT NULL,
+    is_pull_request BOOL        NOT NULL,
+    title           TEXT        NOT NULL,
+    body            TEXT        NOT NULL,
+    labels          TEXT[]      NOT NULL,
+    created_at      TIMESTAMPTZ NOT NULL,
+    closed_at       TIMESTAMPTZ,
+    PRIMARY KEY (owner, repo, issue_number)
+);
 
 CREATE UNIQUE INDEX ON github_issue (owner, repo, issue_number);
 CREATE INDEX ON github_issue (state);
 CREATE INDEX ON github_issue (labels);
+
+CREATE OR REPLACE PROCEDURE regenerate_github_issue(target_owner TEXT, target_repo TEXT, target_issue_number INTEGER)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO github_issue(owner,
+                             repo,
+                             issue_number,
+                             timestamp,
+                             state,
+                             is_pull_request,
+                             title,
+                             body,
+                             labels,
+                             created_at,
+                             closed_at)
+    WITH github_issue_label AS (
+        SELECT owner, repo, issue_number, jsonb_array_elements(data -> 'labels') ->> 'name' AS label
+        FROM github_issue_data
+    ),
+         github_issue_labels AS (
+             SELECT owner, repo, issue_number, array_agg(label) AS labels
+             FROM github_issue_label
+             GROUP BY owner, repo, issue_number
+         )
+    SELECT i.owner,
+           i.repo,
+           i.issue_number,
+           i.timestamp,
+           i.data ->> 'state',
+           i.data -> 'pull_request' IS NOT NULL,
+           COALESCE(i.data ->> 'title', ''),
+           COALESCE(i.data ->> 'body', ''),
+           COALESCE(il.labels, '{}'),
+           (data ->> 'created_at')::TIMESTAMPTZ,
+           (data ->> 'closed_at')::TIMESTAMPTZ
+    FROM github_issue_data i
+             LEFT JOIN github_issue_labels il
+                       ON i.owner = il.owner AND i.repo = il.repo AND i.issue_number = il.issue_number
+    WHERE i.owner = target_owner
+      AND i.repo = target_repo
+      AND i.issue_number = target_issue_number
+    ON CONFLICT (owner, repo, issue_number) DO UPDATE
+        SET timestamp       = excluded.timestamp,
+            state           = excluded.state,
+            is_pull_request = excluded.is_pull_request,
+            title           = excluded.title,
+            body            = excluded.body,
+            labels          = excluded.labels,
+            created_at      = excluded.created_at,
+            closed_at       = excluded.closed_at;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION trigger_function_regenerate_github_issue()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NEW IS NULL THEN
+        DELETE FROM github_issue WHERE owner = OLD.owner AND repo = OLD.repo AND issue_number = OLD.issue_number;
+    ELSE
+        CALL regenerate_github_issue(NEW.owner, NEW.repo, NEW.issue_number);
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trigger_regenerate_github_issue
+    AFTER INSERT OR UPDATE OR DELETE
+    ON github_issue_data
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_function_regenerate_github_issue();
 
 
 CREATE VIEW github_issue_label AS
@@ -45,32 +117,19 @@ SELECT owner, repo, issue_number, unnest(labels) AS label
 FROM github_issue;
 
 
-CREATE MATERIALIZED VIEW github_label AS
-WITH github_label_data AS (
-    SELECT owner, repo, jsonb_array_elements(data #> '{labels}') AS data
-    FROM github_issue_data
-),
-     _github_label AS (
-         SELECT owner,
-                repo,
-                data ->> 'name'                                               AS name,
-                data ->> 'description'                                        AS description,
-                row_number() OVER (PARTITION BY owner, repo, data ->> 'name') AS row_number
-         FROM github_label_data
-     )
-SELECT owner, repo, name, description
-FROM _github_label
-WHERE row_number = 1;
+CREATE VIEW github_label AS
+WITH github_label AS (
+    SELECT owner, repo, unnest(labels) AS name
+    FROM github_issue
+)
+SELECT owner, repo, name
+FROM github_label
+GROUP BY owner, repo, name;
 
-CREATE UNIQUE INDEX ON github_label (owner, repo, name);
-
-
-CREATE MATERIALIZED VIEW github_label_team AS
+CREATE VIEW github_label_team AS
 SELECT owner, repo, SUBSTR(name, 6) AS name, name as label
 FROM github_label
 WHERE name LIKE 'team-%';
-
-CREATE UNIQUE INDEX ON github_label_team (owner, repo, label);
 
 
 CREATE VIEW github_label_team_issue AS
@@ -146,14 +205,3 @@ FROM github_label_team_issue_open gltio
          LEFT JOIN github_label_team_issue_open_no_priority gltionp
                    ON gltio.owner = gltionp.owner AND gltio.repo = gltionp.repo AND gltio.team = gltionp.team;
 
-
-CREATE OR REPLACE PROCEDURE refresh_github_issue_materialized_views()
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    REFRESH MATERIALIZED VIEW CONCURRENTLY github_issue;
-    REFRESH MATERIALIZED VIEW CONCURRENTLY github_label;
-    REFRESH MATERIALIZED VIEW CONCURRENTLY github_label_team;
-END;
-$$;
