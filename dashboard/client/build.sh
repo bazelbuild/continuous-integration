9e417be43ff654b2bf41461c34519614211905c2
Chi Wang <coeuvre@gmail.com>
2021-01-23 13:39:29 +0800

    Add Dockerfile for dashboard client

---
+++ dashboard/client/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+GIT_REV=$(git rev-parse --short HEAD)
+
+docker build -f Dockerfile -t gcr.io/bazel-public/dashboard-client:$GIT_REV .
--- dashboard/server/build.sh
+++ dashboard/server/build.sh
@@ -8,4 +8,4 @@ GIT_REV=$(git rev-parse --short HEAD)
 
 rm -rf target/dependency && mkdir -p target/dependency && (cd target/dependency; jar -xf ../dashboard-${GIT_REV}.jar)
 
-docker build -f Dockerfile -t gcr.io/bazel-public/dashboard:$GIT_REV target
+docker build -f Dockerfile -t gcr.io/bazel-public/dashboard-server:$GIT_REV target
