a213ef883f846a9652eec5b980dd12836034aa86
Chi Wang <chiwang@google.com>
2021-01-23 17:25:01 +0800

    Add SERVER_URL to client

--- dashboard/client/next.config.js
+++ dashboard/client/next.config.js
@@ -2,12 +2,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const serverUrl = process.env.SERVER_URL || "http://localhost:8080"
+
 module.exports = withBundleAnalyzer({
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/:path*",
+        destination: serverUrl + "/:path*",
       },
     ];
   },
