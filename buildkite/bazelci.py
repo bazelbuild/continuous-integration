d0b81dfd1ccb44fef66b56281de9c7ffe14527af
Yun Peng <pcloudy@google.com>
2021-01-27 18:16:03 +0100

    Re-enable rules_appengine

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -263,7 +263,6 @@ DOWNSTREAM_PROJECTS_PRODUCTION = {
         "git_repository": "https://github.com/bazelbuild/rules_appengine.git",
         "http_config": "https://raw.githubusercontent.com/bazelbuild/rules_appengine/master/.bazelci/presubmit.yml",
         "pipeline_slug": "rules-appengine-appengine",
-        "disabled_reason": "https://github.com/bazelbuild/rules_appengine/issues/119",
     },
     "rules_apple": {
         "git_repository": "https://github.com/bazelbuild/rules_apple.git",
