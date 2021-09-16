6267ead74a972df80a1d0a139119854a20231b6e
Chi Wang <chiwang@google.com>
2021-09-16 16:24:24 +0800

    Add tests for non windows

--- agent/tests/artifact/upload.rs
+++ agent/tests/artifact/upload.rs
@@ -19,3 +19,21 @@ fn test_logs_uploaded_to_buildkite() -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(not(target_os = "windows"))]
+#[test]
+fn test_logs_uploaded_to_buildkite() -> Result<()> {
+    let mut cmd = Command::cargo_bin("bazelci-agent")?;
+    cmd.args([
+        "artifact",
+        "upload",
+        "--dry",
+        "--mode=buildkite",
+        "--build_event_json_file=tests/data/test_bep.json",
+    ]);
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("buildkite-agent artifact upload bazel-out/darwin-fastbuild/testlogs/src/test/shell/bazel/starlark_repository_test/shard_4_of_6/test_attempts/attempt_1.log"));
+
+    Ok(())
+}
