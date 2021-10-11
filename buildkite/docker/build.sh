80782944bcc38cc9bbaef4a8f61cdf8b50b2d242
Philipp Wollermann <philwo@google.com>
2021-10-11 17:40:02 +0200

    Speed-up buildkite/docker/build.sh
    Change-Id: I963199a138def6d6a1b4d437b51d8080831dd797

--- buildkite/docker/build.sh
+++ buildkite/docker/build.sh
@@ -19,13 +19,15 @@ esac
 export DOCKER_BUILDKIT=1
 
 # Containers used by Bazel CI
-docker build -f centos7/Dockerfile    --target centos7-java8           -t "gcr.io/$PREFIX/centos7-java8" centos7
+docker build -f centos7/Dockerfile    --target centos7-java8           -t "gcr.io/$PREFIX/centos7-java8" centos7 &
+docker build -f debian10/Dockerfile   --target debian10-java11         -t "gcr.io/$PREFIX/debian10-java11" debian10 &
+docker build -f ubuntu1604/Dockerfile --target ubuntu1604-java8        -t "gcr.io/$PREFIX/ubuntu1604-java8" ubuntu1604 &
+docker build -f ubuntu1804/Dockerfile --target ubuntu1804-java11       -t "gcr.io/$PREFIX/ubuntu1804-java11" ubuntu1804 &
+docker build -f ubuntu2004/Dockerfile --target ubuntu2004-java11       -t "gcr.io/$PREFIX/ubuntu2004-java11" ubuntu2004 &
+wait
+
 docker build -f centos7/Dockerfile    --target centos7-releaser        -t "gcr.io/$PREFIX/centos7-releaser" centos7
-docker build -f debian10/Dockerfile   --target debian10-java11         -t "gcr.io/$PREFIX/debian10-java11" debian10
 docker build -f ubuntu1604/Dockerfile --target ubuntu1604-bazel-java8  -t "gcr.io/$PREFIX/ubuntu1604-bazel-java8" ubuntu1604
-docker build -f ubuntu1604/Dockerfile --target ubuntu1604-java8        -t "gcr.io/$PREFIX/ubuntu1604-java8" ubuntu1604
 docker build -f ubuntu1804/Dockerfile --target ubuntu1804-bazel-java11 -t "gcr.io/$PREFIX/ubuntu1804-bazel-java11" ubuntu1804
-docker build -f ubuntu1804/Dockerfile --target ubuntu1804-java11       -t "gcr.io/$PREFIX/ubuntu1804-java11" ubuntu1804
 docker build -f ubuntu2004/Dockerfile --target ubuntu2004-bazel-java11 -t "gcr.io/$PREFIX/ubuntu2004-bazel-java11" ubuntu2004
-docker build -f ubuntu2004/Dockerfile --target ubuntu2004-java11       -t "gcr.io/$PREFIX/ubuntu2004-java11" ubuntu2004
 docker build -f ubuntu2004/Dockerfile --target ubuntu2004-java11-kythe -t "gcr.io/$PREFIX/ubuntu2004-java11-kythe" ubuntu2004
