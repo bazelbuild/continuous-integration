ba846cffda98ec33e7604c1df6d151aebdf1d242
Philipp Wollermann <philwo@google.com>
2021-10-20 22:32:22 +0200

    Update push.sh with new containers.

--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -16,8 +16,11 @@ esac
 
 # Containers used by Bazel CI
 docker push "gcr.io/$PREFIX/centos7-java8" &
+docker push "gcr.io/$PREFIX/centos7-java11" &
+docker push "gcr.io/$PREFIX/centos7-java11-devtoolset10" &
 docker push "gcr.io/$PREFIX/centos7-releaser" &
 docker push "gcr.io/$PREFIX/debian10-java11" &
+docker push "gcr.io/$PREFIX/debian11-java17" &
 docker push "gcr.io/$PREFIX/ubuntu1604-bazel-java8" &
 docker push "gcr.io/$PREFIX/ubuntu1604-java8" &
 docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
@@ -25,4 +28,6 @@ docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-java11-kythe" &
+docker push "gcr.io/$PREFIX/ubuntu2104-java11" &
+docker push "gcr.io/$PREFIX/ubuntu2110-java17" &
 wait
