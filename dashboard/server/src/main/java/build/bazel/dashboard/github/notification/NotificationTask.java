98dd9ae886b69b527c125dd4501e1d4e8fcc92c5
Chi Wang <chiwang@google.com>
2021-12-06 12:08:49 +0100

    Add tags to notification emails

--- dashboard/server/src/main/java/build/bazel/dashboard/github/notification/NotificationTask.java
+++ dashboard/server/src/main/java/build/bazel/dashboard/github/notification/NotificationTask.java
@@ -2,6 +2,7 @@ package build.bazel.dashboard.github.notification;
 
 import build.bazel.dashboard.config.DashboardConfig;
 import build.bazel.dashboard.github.issue.GithubIssue;
+import build.bazel.dashboard.github.issue.GithubIssue.Label;
 import build.bazel.dashboard.github.issuecomment.GithubIssueCommentService;
 import build.bazel.dashboard.github.issuelist.GithubIssueList;
 import build.bazel.dashboard.github.issuelist.GithubIssueListService;
@@ -11,18 +12,16 @@ import build.bazel.dashboard.github.user.GithubUser;
 import build.bazel.dashboard.github.user.GithubUserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
+import java.awt.Color;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -32,6 +31,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,43 +53,84 @@ public class NotificationTask {
     startNotifyIssueStatus();
   }
 
+  @GetMapping("/internal/github/issues/notifications/triage")
+  public Single<String> getTriageTeamNotificationBody() {
+    return buildTriageTeamNotificationBody();
+  }
+
+  @GetMapping("/internal/github/issues/notifications/{user}")
+  public Single<String> getGithubUserNotificationBody(@PathVariable("user") String username) {
+    return githubUserService
+        .findAll()
+        .filter(user -> user.getUsername().equals(username))
+        .concatMapSingle(this::buildUserNotificationBody)
+        .collect(Collectors.joining());
+  }
+
+  @PostMapping("/internal/github/issues/notifications/{user}")
+  public Completable notifyGithubUser(@PathVariable("user") String username) {
+    return githubUserService
+        .findAll()
+        .filter(user -> user.getUsername().equals(username))
+        .flatMapCompletable(
+            user ->
+                buildUserNotificationBody(user)
+                    .flatMapCompletable(
+                        body -> {
+                          if (!body.isBlank()) {
+                            return Completable.fromCallable(
+                                () -> {
+                                  sendNotification(user.getEmail(), body, "triage/update");
+                                  return null;
+                                });
+                          }
+
+                          return Completable.complete();
+                        }));
+  }
+
   @Scheduled(cron = "0 0 2 * * MON-FRI", zone = "UTC")
   public void startNotifyIssueStatus() {
     notifyTriageTeam().andThen(notifyUsers()).blockingAwait();
   }
 
-  Completable notifyTriageTeam() {
+  Single<String> buildTriageTeamNotificationBody() {
     ListParams params = new ListParams();
     params.setOwner("bazelbuild");
     params.setRepo("bazel");
     params.setStatus(GithubIssueStatus.Status.TO_BE_REVIEWED);
-
     return githubIssueListService
         .find(params)
-        .flatMapCompletable(
+        .flatMap(
             list -> {
               if (list.getTotal() > 0) {
                 String reviewLink =
                     dashboardConfig.getHost()
                         + "/issues?q=%7B%22status%22%3A%22TO_BE_REVIEWED%22%2C%22page%22%3A1%7D";
-                return buildNotificationBody(reviewLink, "issues", "review", list)
-                    .flatMapCompletable(
-                        body ->
-                            Completable.fromCallable(
-                                () -> {
-                                  sendNotification(
-                                      dashboardConfig
-                                          .getGithub()
-                                          .getNotification()
-                                          .getToNeedReviewEmail(),
-                                      body,
-                                      "review");
-
-                                  return null;
-                                }));
+                return buildNotificationBody(reviewLink, "issues", "review", list);
+              } else {
+                return Single.just("");
               }
+            });
+  }
 
-              return Completable.complete();
+  Completable notifyTriageTeam() {
+    return buildTriageTeamNotificationBody()
+        .flatMapCompletable(
+            body -> {
+              if (!Strings.isNullOrEmpty(body)) {
+                return Completable.fromCallable(
+                    () -> {
+                      sendNotification(
+                          dashboardConfig.getGithub().getNotification().getToNeedReviewEmail(),
+                          body,
+                          "review");
+
+                      return null;
+                    });
+              } else {
+                return Completable.complete();
+              }
             });
   }
 
@@ -149,6 +191,55 @@ public class NotificationTask {
             });
   }
 
+  private static class Rgb {
+    public int r;
+    public int g;
+    public int b;
+  }
+
+  private static Rgb hexToRgb(String hex) {
+    Rgb rgb = new Rgb();
+    rgb.r = Integer.parseInt(hex.substring(0, 2), 16);
+    rgb.g = Integer.parseInt(hex.substring(2, 4), 16);
+    rgb.b = Integer.parseInt(hex.substring(4, 6), 16);
+    return rgb;
+  }
+
+  private void appendLabel(StringBuilder s, Label label) {
+    Rgb rgb = hexToRgb(label.getColor());
+    float[] hsl = Color.RGBtoHSB(rgb.r, rgb.g, rgb.b, null);
+    float perceivedLightness = ((rgb.r * 0.2126f) + (rgb.g * 0.7152f) + (rgb.b * 0.0722f)) / 255;
+    float lightnessThreshold = 0.453f;
+    float borderThreshold = 0.96f;
+    float lightnessSwitch =
+        Math.max(0, Math.min((perceivedLightness - lightnessThreshold) * -1000, 1));
+    float borderAlpha = Math.max(0, Math.min((perceivedLightness - borderThreshold) * 100, 1));
+
+    s.append("<span style=\"");
+    s.append("display: inline-block; padding: 0 7px; margin-right: 4px;");
+    s.append("border: 1px solid; border-radius: 2em;");
+    s.append("background: rgb(")
+        .append(rgb.r)
+        .append(", ")
+        .append(rgb.g)
+        .append(", ")
+        .append(rgb.b)
+        .append(");");
+    s.append("color: hsl(0, 0%, calc(").append(lightnessSwitch).append(" * 100%));");
+    s.append("border-color: hsla(")
+        .append(hsl[0])
+        .append(", calc(")
+        .append(hsl[1])
+        .append(" * 1%), calc((")
+        .append(hsl[1])
+        .append(" - 25) * 1%), ")
+        .append(borderAlpha)
+        .append(");");
+    s.append("\">");
+    s.append(label.getName());
+    s.append("</span>");
+  }
+
   private Maybe<String> buildIssueListItem(GithubIssueList.Item issue) {
     GithubIssue.Data data;
     try {
@@ -161,7 +252,7 @@ public class NotificationTask {
         .map(
             participants -> {
               StringBuilder body = new StringBuilder();
-              body.append("<tr>");
+              body.append("<tr style=\"vertical-align: baseline\">");
 
               body.append("<td>");
               appendLink(
@@ -172,7 +263,7 @@ public class NotificationTask {
                   "#" + issue.getIssueNumber());
               body.append("</td>");
 
-              body.append("<td>");
+              body.append("<td style=\"white-space: nowrap;\">");
               String title = data.getTitle();
               if (title.length() > 80) {
                 title = title.substring(0, 77) + "...";
@@ -195,7 +286,14 @@ public class NotificationTask {
               }
               body.append("</td>");
 
+              body.append("<td>");
+              for (Label label : data.getLabels()) {
+                appendLabel(body, label);
+              }
+              body.append("</td>");
+
               body.append("</tr>");
+
               return body.toString();
             })
         .toMaybe();
@@ -223,17 +321,21 @@ public class NotificationTask {
     sb.append("</a>");
   }
 
-  Completable notifyUsers() {
+  private Single<String> buildUserNotificationBody(GithubUser user) {
+    return Flowable.concatArray(
+            buildNeedTriageMessage(user).toFlowable(),
+            buildFixP0BugsMessage(user).toFlowable(),
+            buildFixP1BugsMessage(user).toFlowable(),
+            buildFixP2BugsMessage(user).toFlowable())
+        .collect(Collectors.joining());
+  }
+
+  private Completable notifyUsers() {
     return githubUserService
         .findAll()
         .flatMapCompletable(
             user ->
-                Flowable.concatArray(
-                        buildNeedTriageMessage(user).toFlowable(),
-                        buildFixP0BugsMessage(user).toFlowable(),
-                        buildFixP1BugsMessage(user).toFlowable(),
-                        buildFixP2BugsMessage(user).toFlowable())
-                    .collect(Collectors.joining())
+                buildUserNotificationBody(user)
                     .flatMapCompletable(
                         body -> {
                           if (!body.isBlank()) {
