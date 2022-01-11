5815203552706c27004a54f8f412d9e3776f7948
Yun Peng <pcloudy@google.com>
2022-01-11 13:58:53 +0100

    Fix LINUX_BINARY_PLATFORM

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -575,7 +575,7 @@ DEFAULT_PLATFORM = "ubuntu1804"
 # In order to test that "the one Linux binary" that we build for our official releases actually
 # works on all Linux distributions that we test on, we use the Linux binary built on our official
 # release platform for all Linux downstream tests.
-LINUX_BINARY_PLATFORM = "centos7_java11_devtoolset10"
+LINUX_BINARY_PLATFORM = "centos7"
 
 DEFAULT_XCODE_VERSION = "13.0"
 XCODE_VERSION_REGEX = re.compile(r"^\d+\.\d+(\.\d+)?$")
