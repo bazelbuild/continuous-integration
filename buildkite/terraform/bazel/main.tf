3089e7274253decfaf6868cc55aa0737c91dffd9
Florian Weikert <fwe@google.com>
2022-06-02 14:42:38 +0200

    Add crubit pipeline to terraform config
    Progress towards #1372

--- buildkite/terraform/bazel/main.tf
+++ buildkite/terraform/bazel/main.tf
@@ -1900,3 +1900,20 @@ resource "buildkite_pipeline" "bazel-arm64" {
   }
 }
 
+resource "buildkite_pipeline" "crubit" {
+  name = "crubit"
+  repository = "https://github.com/google/crubit.git"
+  steps = templatefile("pipeline.yml.tpl", { envs = {}, steps = { commands = ["curl -sS \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/bazelci.py?$(date +%s)\" -o bazelci.py", "python3.6 bazelci.py project_pipeline | tee /dev/tty | buildkite-agent pipeline upload"] } })
+  default_branch = "main"
+  team = [{ access_level = "MANAGE_BUILD_AND_READ", slug = "googlers" }, { access_level = "BUILD_AND_READ", slug = "bazel" }]
+  provider_settings {
+    trigger_mode = "code"
+    build_pull_requests = true
+    skip_pull_request_builds_for_existing_commits = true
+    build_pull_request_forks = true
+    prefix_pull_request_fork_branch_names = true
+    build_branches = true
+    publish_commit_status = true
+    separate_pull_request_statuses = true
+  }
+}
