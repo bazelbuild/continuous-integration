f203bf01d8a30ba33965a2bc66caecb96668d766
Yun Peng <pcloudy@google.com>
2022-07-06 19:15:15 +0200

    Update bazel-trusted config to match the current status

--- buildkite/terraform/bazel-trusted/main.tf
+++ buildkite/terraform/bazel-trusted/main.tf
@@ -41,7 +41,7 @@ resource "buildkite_pipeline" "docgen-bazel-website" {
   branch_configuration = "master"
   team = [{ access_level = "MANAGE_BUILD_AND_READ", slug = "bazel-docs" }]
   provider_settings {
-    trigger_mode = "code"
+    trigger_mode = "none"
     skip_pull_request_builds_for_existing_commits = true
     prefix_pull_request_fork_branch_names = true
     build_branches = true
@@ -67,11 +67,11 @@ resource "buildkite_pipeline" "docgen-bazel" {
   name = "DocGen: Bazel"
   repository = "https://github.com/bazelbuild/bazel.git"
   steps = templatefile("pipeline.yml.tpl", { envs = {}, steps = { commands = ["curl -s \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/pipelines/bazel-docgen.yml?$(date +%s)\" | tee /dev/tty | buildkite-agent pipeline upload --replace"] } })
-  default_branch = "master"
-  branch_configuration = "master"
+  default_branch = "5.1-docs-fixes"
+  branch_configuration = "5.1-docs-fixes"
   team = [{ access_level = "MANAGE_BUILD_AND_READ", slug = "bazel-docs" }]
   provider_settings {
-    trigger_mode = "code"
+    trigger_mode = "none"
     skip_pull_request_builds_for_existing_commits = true
     prefix_pull_request_fork_branch_names = true
     build_branches = true
