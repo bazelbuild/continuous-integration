ed13bd4d9b413df57a59c124abcbc7c86d91c8d1
Yun Peng <pcloudy@google.com>
2023-05-08 17:04:22 +0200

    Use default .bazelci/presubmit.yml file for rules_android

--- buildkite/terraform/bazel/main.tf
+++ buildkite/terraform/bazel/main.tf
@@ -1168,7 +1168,7 @@ resource "buildkite_pipeline" "bazel-bazel-github-presubmit" {
 resource "buildkite_pipeline" "rules-android" {
   name = "rules_android"
   repository = "https://github.com/bazelbuild/rules_android.git"
-  steps = templatefile("pipeline.yml.tpl", { envs = {}, steps = { commands = ["curl -sS \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/bazelci.py?$(date +%s)\" -o bazelci.py", "python3.6 bazelci.py project_pipeline --http_config=https://raw.githubusercontent.com/bazelbuild/rules_android/master/.bazelci/postsubmit.yml | tee /dev/tty | buildkite-agent pipeline upload"] } })
+  steps = templatefile("pipeline.yml.tpl", { envs = {}, steps = { commands = ["curl -sS \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/bazelci.py?$(date +%s)\" -o bazelci.py", "python3.6 bazelci.py project_pipeline | tee /dev/tty | buildkite-agent pipeline upload"] } })
   description = "Android rules for Bazel"
   default_branch = "master"
   team = [{ access_level = "BUILD_AND_READ", slug = "bazel" }, { access_level = "MANAGE_BUILD_AND_READ", slug = "android-team" }]
