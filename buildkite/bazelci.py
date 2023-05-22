fa0bc1298c9573b2a20d15d639356e4702ce4692
Chi Wang <chiwang@google.com>
2023-05-22 16:12:38 +0200

    Use agent 0.2.1

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -1729,7 +1729,7 @@ def download_bazelci_agent(dest_dir):
     repo = "bazelbuild/continuous-integration"
     if THIS_IS_TESTING and "BAZELCI_AGENT_REPO" in os.environ:
         repo = os.environ["BAZELCI_AGENT_REPO"]
-    version = "0.2.0"
+    version = "0.2.1"
     if THIS_IS_TESTING and "BAZELCI_AGENT_VERSION" in os.environ:
         version = os.environ["BAZELCI_AGENT_VERSION"]
     postfix = ""
