308097df57a6e1dd4cab68bfff5ba48764ddb82f
Chi Wang <chiwang@google.com>
2023-10-18 14:22:58 +0200

    [rbe-configs] Extract configs out of generate.py and add entry for 6.3.2 (#1763)

---
+++ rbe-configs/data.py
@@ -0,0 +1,263 @@
+configs = [
+  {
+    "bazel_version": "6.3.2",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2204-bazel-java17',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2204-java17',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "5.2.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "5.1.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "5.0.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "4.2.2",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "4.2.1",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "4.2.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "4.1.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+  {
+    "bazel_version": "4.0.0",
+    "containers": [
+      {
+          'toolchain_name': 'ubuntu1604-bazel-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1604-java8',
+          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu1804-java11',
+          'cpp_env_json': 'cpp_env/ubuntu1804.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-bazel-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+      {
+          'toolchain_name': 'ubuntu2004-java11',
+          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+      },
+    ]
+  },
+]
