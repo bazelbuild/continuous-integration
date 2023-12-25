1676e530bc2a61e150fff2a6dc24ccfbb881a2bf
iancha1992 <heec@google.com>
2023-12-24 18:11:55 -0800

    Testing

--- actions/lockfile-test/update_lockfile.py
+++ actions/lockfile-test/update_lockfile.py
@@ -1,5 +1,6 @@
 import os, requests, subprocess
 token = os.environ["GH_TOKEN"]
+github_dir = os.environ["INPUT_GITHUB_DIR"]
 
 print("Cloning and syncing the repo...")
 # subprocess.run(['gh', 'repo', 'sync', gh_cli_repo_name, "-b", master_branch])
@@ -10,3 +11,8 @@ subprocess.run(['git', 'config', '--global', 'user.email', "bazel-io-bot@google.
 os.chdir("bazel")
 subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
 subprocess.run(['git', 'remote', '-v'])
+
+subprocess.run(["", ""])
+
+# "${GITHUB_WORKSPACE}/bin/bazel" run //src/test/tools/bzlmod:update_default_lock_file
+# "${GITHUB_WORKSPACE}/bin/bazel" mod deps --lockfile_mode=update
