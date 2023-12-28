32d38417b8042d5b83c83e6e3161448f0ef7662b
iancha1992 <heec@google.com>
2023-12-27 16:50:22 -0800

    Testing

--- actions/lockfile-test/update_lockfile.py
+++ actions/lockfile-test/update_lockfile.py
@@ -1,38 +1,13 @@
 import os, requests, subprocess
 token = os.environ["GH_TOKEN"]
-
-print("Cloning and syncing the repo...")
-subprocess.run(['git', 'clone', f"https://bazel-io:{token}@github.com/iancha1992/bazel.git"])
-subprocess.run(['git', 'config', '--global', 'user.name', "bazel-io"])
-subprocess.run(['git', 'config', '--global', 'user.email', "bazel-io-bot@google.com"])
-os.chdir("bazel")
-subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
-subprocess.run(['git', 'remote', '-v'])
-
-print("checking branches")
-subprocess.run(['git', 'branch'])
-
-# subprocess.run(["../bazelisk-linux-amd64", "run", "//src/test/tools/bzlmod:update_default_lock_file"])
-print("Create hiword.txt")
-subprocess.run(["touch", "hiword.txt"])
-subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit", "-m", "Testing!"])
-subprocess.run(["git", "push"])
-
-
-
-
-
-
-
-# New code here
+pr_number = os.environ["PR_NUMBER"]
 
 # print("Cloning and syncing the repo...")
-# subprocess.run(['git', 'clone', f"https://bazel-io:{token}@github.com/bazelbuild/bazel.git"])
+# subprocess.run(['git', 'clone', f"https://bazel-io:{token}@github.com/iancha1992/bazel.git"])
 # subprocess.run(['git', 'config', '--global', 'user.name', "bazel-io"])
 # subprocess.run(['git', 'config', '--global', 'user.email', "bazel-io-bot@google.com"])
 # os.chdir("bazel")
-# # subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
+# subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
 # subprocess.run(['git', 'remote', '-v'])
 
 # print("checking branches")
@@ -44,3 +19,34 @@ subprocess.run(["git", "push"])
 # subprocess.run(["git", "add", "."])
 # subprocess.run(["git", "commit", "-m", "Testing!"])
 # subprocess.run(["git", "push"])
+
+
+
+
+print("This is the pr number", pr_number)
+
+
+# New code here
+headers = {
+    'X-GitHub-Api-Version': '2022-11-28'
+}
+r = requests.get(f'https://api.github.com/repos/bazelbuild/bazel/pulls/', headers=headers).json()
+
+
+print("Cloning and syncing the repo...")
+subprocess.run(['git', 'clone', f"https://bazel-io:{token}@github.com/bazelbuild/bazel.git"])
+subprocess.run(['git', 'config', '--global', 'user.name', "bazel-io"])
+subprocess.run(['git', 'config', '--global', 'user.email', "bazel-io-bot@google.com"])
+os.chdir("bazel")
+# subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
+subprocess.run(['git', 'remote', '-v'])
+
+print("checking branches")
+subprocess.run(['git', 'branch'])
+
+# subprocess.run(["../bazelisk-linux-amd64", "run", "//src/test/tools/bzlmod:update_default_lock_file"])
+print("Create hiword.txt")
+subprocess.run(["touch", "hiword.txt"])
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", "Testing!"])
+subprocess.run(["git", "push"])
