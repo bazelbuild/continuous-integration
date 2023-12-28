88069daa742c6cf1d8c8eeff75c6598ed12886ba
iancha1992 <heec@google.com>
2023-12-27 16:26:39 -0800

    Testing

--- actions/lockfile-test/update_lockfile.py
+++ actions/lockfile-test/update_lockfile.py
@@ -19,3 +19,28 @@ subprocess.run(["git", "add", "."])
 subprocess.run(["git", "commit", "-m", "Testing!"])
 subprocess.run(["git", "push"])
 
+
+
+
+
+
+
+# New code here
+
+# print("Cloning and syncing the repo...")
+# subprocess.run(['git', 'clone', f"https://bazel-io:{token}@github.com/bazelbuild/bazel.git"])
+# subprocess.run(['git', 'config', '--global', 'user.name', "bazel-io"])
+# subprocess.run(['git', 'config', '--global', 'user.email', "bazel-io-bot@google.com"])
+# os.chdir("bazel")
+# # subprocess.run(['git', 'remote', 'add', 'origin', "git@github.com:iancha1992/bazel.git"])
+# subprocess.run(['git', 'remote', '-v'])
+
+# print("checking branches")
+# subprocess.run(['git', 'branch'])
+
+# # subprocess.run(["../bazelisk-linux-amd64", "run", "//src/test/tools/bzlmod:update_default_lock_file"])
+# print("Create hiword.txt")
+# subprocess.run(["touch", "hiword.txt"])
+# subprocess.run(["git", "add", "."])
+# subprocess.run(["git", "commit", "-m", "Testing!"])
+# subprocess.run(["git", "push"])
