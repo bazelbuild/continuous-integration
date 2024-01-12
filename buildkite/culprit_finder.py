bazelbuild/continuous-integration

commit fb86f6792dd3e4b896edf738312ac7684cd63496

Allow downstream projects configs to be file_config (#1778)
Reapply https://github.com/bazelbuild/continuous-integration/pull/1776
with fixes:

- We need to properly clone the git repository so that we can get
`file_config` at the correct commit.
- Refactored `downstream-root` so that we don't need to get platform
info from task config for cloning the repo, which introduces a
chicken-egg problem.
- Allow suppressing stdout in `clone_git_repository` so that it won't
impact stdout of `project_pipeline`
- Make `validate_config` also work for downstream projects by adding
`git_commit` as a parameter of `get_modified_files`.

buildkite/culprit_finder.py
@@ -57,8 +57,9 @@ def get_bazel_commits_between(first_commit, second_commit):
 
 
 def get_configs(project_name):
-    http_config = bazelci.DOWNSTREAM_PROJECTS[project_name]["http_config"]
-    configs = bazelci.fetch_configs(http_config, None)
+    http_config = bazelci.DOWNSTREAM_PROJECTS[project_name].get("http_config")
+    file_config = bazelci.DOWNSTREAM_PROJECTS[project_name].get("file_config")
+    configs = bazelci.fetch_configs(http_config, file_config)
     return configs
 
 
@@ -76,7 +77,8 @@ def get_tasks(project_name):
 def test_with_bazel_at_commit(
     project_name, task_name, repo_location, bazel_commit, needs_clean, repeat_times
 ):
-    http_config = bazelci.DOWNSTREAM_PROJECTS[project_name]["http_config"]
+    http_config = bazelci.DOWNSTREAM_PROJECTS[project_name].get("http_config")
+    file_config = bazelci.DOWNSTREAM_PROJECTS[project_name].get("file_config")
     for i in range(1, repeat_times + 1):
         if repeat_times > 1:
             bazelci.print_collapsed_group(":bazel: Try %s time" % i)
@@ -85,10 +87,11 @@ def test_with_bazel_at_commit(
                 [
                     "runner",
                     "--task=" + task_name,
-                    "--http_config=" + http_config,
                     "--repo_location=" + repo_location,
                     "--use_bazel_at_commit=" + bazel_commit,
                 ]
+                + (["--http_config=" + http_config] if http_config else [])
+                + (["--file_config=" + file_config,] if file_config else [])
                 + (["--needs_clean"] if needs_clean else [])
             )
         except subprocess.CalledProcessError as e:
@@ -99,14 +102,10 @@ def test_with_bazel_at_commit(
     return True
 
 
-def clone_git_repository(project_name, task_name):
-    platform_name = get_platform(project_name, task_name)
+def clone_git_repository(project_name, suppress_stdout=False):
     git_repository = bazelci.DOWNSTREAM_PROJECTS[project_name]["git_repository"]
-    last_green_commit_url = bazelci.bazelci_last_green_commit_url(
-        git_repository, bazelci.DOWNSTREAM_PROJECTS[project_name]["pipeline_slug"]
-    )
-    git_commit = bazelci.get_last_green_commit(last_green_commit_url)
-    return bazelci.clone_git_repository(git_repository, platform_name, git_commit)
+    git_commit = bazelci.get_last_green_commit(project_name)
+    return bazelci.clone_git_repository(git_repository, git_commit, suppress_stdout=suppress_stdout)
 
 
 def get_previous_bazel_commit(current_commit, count):
@@ -229,6 +228,15 @@ def main(argv=None):
         try:
             project_name = os.environ["PROJECT_NAME"]
 
+            if project_name not in bazelci.DOWNSTREAM_PROJECTS:
+                raise Exception(
+                    "Project name '%s' not recognized, available projects are %s"
+                    % (project_name, str((bazelci.DOWNSTREAM_PROJECTS.keys())))
+                )
+
+            # Clone the project repo so that we can get its CI config file at the same last green commit.
+            clone_git_repository(project_name, suppress_stdout=True)
+
             # For old config file, we can still set PLATFORM_NAME as task name.
             task = os.environ.get("PLATFORM_NAME") or os.environ.get("TASK_NAME")
             if task:
@@ -242,7 +250,7 @@ def main(argv=None):
             if not good_bazel_commit:
                 # If GOOD_BAZEL_COMMIT is not set, use recorded last bazel green commit for downstream project
                 last_green_commit_url = bazelci.bazelci_last_green_downstream_commit_url()
-                good_bazel_commit = bazelci.get_last_green_commit(last_green_commit_url)
+                good_bazel_commit = bazelci.get_last_green_commit_by_url(last_green_commit_url)
 
             bad_bazel_commit = os.environ.get("BAD_BAZEL_COMMIT")
             if not bad_bazel_commit:
@@ -261,12 +269,6 @@ def main(argv=None):
         if "REPEAT_TIMES" in os.environ:
             repeat_times = int(os.environ["REPEAT_TIMES"])
 
-        if project_name not in bazelci.DOWNSTREAM_PROJECTS:
-            raise Exception(
-                "Project name '%s' not recognized, available projects are %s"
-                % (project_name, str((bazelci.DOWNSTREAM_PROJECTS.keys())))
-            )
-
         print_culprit_finder_pipeline(
             project_name=project_name,
             tasks=tasks,
@@ -276,7 +278,7 @@ def main(argv=None):
             repeat_times=repeat_times,
         )
     elif args.subparsers_name == "runner":
-        repo_location = clone_git_repository(args.project_name, args.task_name)
+        repo_location = clone_git_repository(args.project_name)
         good_bazel_commit, bad_bazel_commit = identify_bisect_range(args, repo_location)
         start_bisecting(
             project_name=args.project_name,

buildkite/bazel-bench/bazel_bench.py
@@ -77,7 +77,6 @@ BAZEL_BENCH_RESULT_FILENAME = "perf_data.csv"
 AGGR_JSON_PROFILES_FILENAME = "aggr_json_profiles.csv"
 PLATFORMS_WHITELIST = ['macos', 'ubuntu1804']
 REPORT_GENERATION_PLATFORM = 'ubuntu1804'
-STARTER_JOB_PLATFORM = 'ubuntu1804'
 
 
 def _bazel_bench_env_setup_command(platform, bazel_commits):
@@ -425,8 +424,7 @@ def main(args=None):
         else datetime.date.today()
     )
 
-    bazel_clone_path = bazelci.clone_git_repository(
-        BAZEL_REPOSITORY, STARTER_JOB_PLATFORM)
+    bazel_clone_path = bazelci.clone_git_repository(BAZEL_REPOSITORY)
     bazel_commits_full_list, bazel_commits_to_benchmark = _get_bazel_commits(
         date, bazel_clone_path, parsed_args.max_commits)
     bazel_bench_ci_steps = []

buildkite/bazel-central-registry/bcr_presubmit.py
@@ -154,16 +154,9 @@ def scratch_file(root, relative_path, lines=None, mode="w"):
     return abspath
 
 
-def get_root_dir(module_name, module_version, task, is_test_module=False):
-    # TODO(pcloudy): We use the "downstream root" as the repo root, find a better root path for BCR presubmit.
-    configs = get_test_module_task_config(module_name, module_version) if is_test_module else get_task_config(module_name, module_version)
-    platform = bazelci.get_platform_for_task(task, configs["tasks"][task])
-    return pathlib.Path(bazelci.downstream_projects_root(platform))
-
-
-def create_simple_repo(module_name, module_version, task):
+def create_simple_repo(module_name, module_version):
     """Create a simple Bazel module repo which depends on the target module."""
-    root = get_root_dir(module_name, module_version, task)
+    root = bazelci.get_repositories_root()
     scratch_file(root, "WORKSPACE")
     scratch_file(root, "BUILD")
     # TODO(pcloudy): Should we test this module as the root module? Maybe we do if we support dev dependency.
@@ -217,10 +210,10 @@ def unpack_archive(archive_file, output_dir):
     else:
         shutil.unpack_archive(archive_file, output_dir)
 
-def prepare_test_module_repo(module_name, module_version, task):
+def prepare_test_module_repo(module_name, module_version):
     """Prepare the test module repo and the presubmit yml file it should use"""
     bazelci.print_collapsed_group(":information_source: Prepare test module repo")
-    root = get_root_dir(module_name, module_version, task, is_test_module = True)
+    root = bazelci.get_repositories_root()
     source = load_source_json(module_name, module_version)
 
     # Download and unpack the source archive to ./output
@@ -420,11 +413,11 @@ def main(argv=None):
             pipeline_steps = [{"block": "Wait on BCR maintainer review", "blocked_state": "running"}] + pipeline_steps
         upload_jobs_to_pipeline(pipeline_steps)
     elif args.subparsers_name == "runner":
-        repo_location = create_simple_repo(args.module_name, args.module_version, args.task)
+        repo_location = create_simple_repo(args.module_name, args.module_version)
         config_file = get_presubmit_yml(args.module_name, args.module_version)
         return run_test(repo_location, config_file, args.task)
     elif args.subparsers_name == "test_module_runner":
-        repo_location, config_file = prepare_test_module_repo(args.module_name, args.module_version, args.task)
+        repo_location, config_file = prepare_test_module_repo(args.module_name, args.module_version)
         return run_test(repo_location, config_file, args.task)
     else:
         parser.print_help()

buildkite/bazel_auto_sheriff.py
@@ -126,11 +126,7 @@ class BuildInfoAnalyzer(threading.Thread):
         job_infos = filter(lambda x: bool(x), (extract_job_info_by_key(job) for job in main_build_info["jobs"] if not ("soft_failed" in job and job["soft_failed"])))
         self.main_result["tasks"] = group_job_info_by_task(job_infos)
         self.main_result["state"] = get_project_state(self.main_result["tasks"])
-
-        last_green_commit_url = bazelci.bazelci_last_green_commit_url(
-            bazelci.DOWNSTREAM_PROJECTS[self.project]["git_repository"], self.pipeline
-        )
-        self.main_result["last_green_commit"] = bazelci.get_last_green_commit(last_green_commit_url)
+        self.main_result["last_green_commit"] = bazelci.get_last_green_commit(self.project)
 
 
     # Log all succeeded projects in the same annotate block

commit 831f3cee471bb263748aea96f946febe83891bf1

Fix culprit_finder.py
Was broken by https://github.com/bazelbuild/continuous-integration/commit/fb86f6792dd3e4b896edf738312ac7684cd63496#diff-ba450f780cf9b79ec952dac692ad5b13fddfcbfbb599acd623a374c35433b660R238 changing the current directory to the project dir

buildkite/culprit_finder.py
@@ -256,7 +256,7 @@ def main(argv=None):
             if not bad_bazel_commit:
                 # If BAD_BAZEL_COMMIT is not set, use HEAD commit.
                 bad_bazel_commit = (
-                    subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+                    subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=BAZEL_REPO_DIR).decode("utf-8").strip()
                 )
         except KeyError as e:
             raise Exception("Environment variable %s must be set" % str(e))