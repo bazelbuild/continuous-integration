8acd4746d3d06cf5ca6684f4a3b7e1437c3e3315
iancha1992 <heec@google.com>
2024-01-24 13:59:51 -0800

    Testing

--- actions/cherry_picker/functions.py
+++ actions/cherry_picker/functions.py
@@ -132,7 +132,7 @@ def update_lockfile():
     print("bazelversion version!!!!")
 
     std_out_bazel_version = subprocess.Popen(["../bazelisk-linux-amd64", "--version"], stdout=subprocess.PIPE)
-    bazel_version = std_out_bazel_version.communicate()
+    bazel_version = std_out_bazel_version.communicate()[0].split(".")
 
     print("hi")
     print(bazel_version)
