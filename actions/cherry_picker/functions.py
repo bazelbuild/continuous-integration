b30fee7b79cf00ffb0cea7b630f0213d86464613
iancha1992 <heec@google.com>
2024-01-24 13:30:39 -0800

    Testing

--- actions/cherry_picker/functions.py
+++ actions/cherry_picker/functions.py
@@ -125,10 +125,16 @@ def cherry_pick(commit_id, release_branch_name, target_branch_name, requires_clo
     run_cherry_pick(input_data["is_prod"], commit_id, target_branch_name)
 
 def update_lockfile():
+    bazelversion_file = open(".bazelversion", "r")
+    print("bazelversion version!!!!")
+    subprocess.run(["../bazelisk-linux-amd64", "--version"])
+
     lockfile_mode_status = subprocess.run(["../bazelisk-linux-amd64", "mod", "deps", "--lockfile_mode=error"])
-    if lockfile_mode_status.returncode != 0:
+    if lockfile_mode_status.returncode == 0: print("The lockfile(s) is already updated")
+    else:
         print("Lockfile(s) may need to be updated...")
         print("Updating the lockfile...")
+        # update_lockfile_status = subprocess.run(["../bazelisk-linux-amd64", "mod", "deps", "--lockfile_mode=update"])
         # subprocess.run(["../bazelisk-linux-amd64", "run", "//src/test/tools/bzlmod:update_default_lock_file"])
         update_lockfile_status = subprocess.run(["../bazelisk-linux-amd64", "mod", "deps", "--lockfile_mode=update"])
         if update_lockfile_status.returncode != 0: raise UpdateLockfileException("Error updating the lockfile...")
