9817b29fbb4b1fda32afb83b29574ec2d491feeb
Yun Peng <pcloudy@google.com>
2024-01-31 17:14:25 +0100

    Avoid broken pipe issue during Linux image creation
    Otherwise, we get error like: "error while communicating with "startup-script" script: bufio.Scanner: token too long" during startup.

--- buildkite/setup-docker.sh
+++ buildkite/setup-docker.sh
@@ -194,7 +194,7 @@ EOF
       "platforms;android-29" \
       "platforms;android-30" \
       "platforms;android-31" \
-      "platforms;android-32"
+      "platforms;android-32" > /dev/null
 }
 
 ### Fix permissions in /opt.
