dd14956b04339e3e2608c0230eaf82b44cf4db9f
Yun Peng <pcloudy@google.com>
2024-02-07 11:44:10 +0100

    Disable bazel-diff if MODULE.bazel is changed
    Prevent breakages like https://buildkite.com/bazel/bazel-bazel/builds/26651

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -598,7 +598,7 @@ AUTO_DIFFBASE_VALUES = frozenset(["1", "true", "auto"])
 
 # Always run all test targets if any of the paths here are modified by the current commit.
 # Values can be directory paths (with a trailing slash) or file paths.
-DISABLE_BAZEL_DIFF_IF_MODIFIED = (".bazelci/", ".bazelversion")
+DISABLE_BAZEL_DIFF_IF_MODIFIED = (".bazelci/", ".bazelversion", "MODULE.bazel")
 
 COMMIT_RE = re.compile(r"[0-9a-z]{40}")
 
