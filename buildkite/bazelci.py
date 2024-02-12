ec38d58bce148afd11c428493bd7d94822929201
Yun Peng <pcloudy@google.com>
2024-02-12 17:07:57 +0100

    Use a custom bazel-postsubmit.yml file for Bazel in downstream pipeline

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -111,7 +111,7 @@ DOWNSTREAM_PROJECTS_PRODUCTION = {
     },
     "Bazel": {
         "git_repository": "https://github.com/bazelbuild/bazel.git",
-        "file_config": ".bazelci/postsubmit.yml",
+        "http_config": "https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/pipelines/bazel-postsubmit.yml",
         "pipeline_slug": "bazel-bazel",
     },
     "Bazel Bench": {
