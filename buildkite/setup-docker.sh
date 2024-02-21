083a1f5a821e8f2d664395c639b69d2c282f2e55
Yun Peng <pcloudy@google.com>
2024-02-21 12:16:03 +0100

    Pull docker images when building the Linux VM image

--- buildkite/setup-docker.sh
+++ buildkite/setup-docker.sh
@@ -209,4 +209,28 @@ EOF
   sleep 3
 }
 
+### Configure and start Docker.
+systemctl start docker
+
+### Ensure that Docker images can be downloaded from GCR.
+gcloud auth configure-docker --quiet
+
+### Pull the Docker images that we need.
+docker pull "gcr.io/bazel-public/centos7-java8" &
+docker pull "gcr.io/bazel-public/centos7-java11" &
+docker pull "gcr.io/bazel-public/centos7-java11-devtoolset10" &
+docker pull "gcr.io/bazel-public/centos7-releaser" &
+docker pull "gcr.io/bazel-public/debian10-java11" &
+docker pull "gcr.io/bazel-public/debian11-java17" &
+docker pull "gcr.io/bazel-public/ubuntu1604-bazel-java8" &
+docker pull "gcr.io/bazel-public/ubuntu1604-java8" &
+docker pull "gcr.io/bazel-public/ubuntu1804-bazel-java11" &
+docker pull "gcr.io/bazel-public/ubuntu1804-java11" &
+docker pull "gcr.io/bazel-public/ubuntu2004-bazel-java11" &
+docker pull "gcr.io/bazel-public/ubuntu2004-java11" &
+docker pull "gcr.io/bazel-public/ubuntu2004-java11-kythe" &
+docker pull "gcr.io/bazel-public/ubuntu2204-java17" &
+docker pull "gcr.io/bazel-public/ubuntu2204-bazel-java17" &
+wait
+
 poweroff
