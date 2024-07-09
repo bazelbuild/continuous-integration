68d838fe066390094621ba8078b4436034bca835
Yun Peng <pcloudy@google.com>
2024-07-09 10:35:39 +0200

    Fix quoting (#1993)

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -3072,9 +3072,9 @@ def show_gerrit_review_link(git_repository, pipeline_steps):
     if not host:
         raise BuildkiteException("Couldn't get host name from %s" % git_repository)
     commit = os.getenv("BUILDKITE_COMMIT")
-    text = f"- The transformed code used in this pipeline can be found under https://{host}-review.googlesource.com/q/{commit}\\n" \
-           f"- Fetch the source with `git fetch https://{host}.googlesource.com/bazel {commit}  && git checkout FETCH_HEAD`"
-    commands = ['buildkite-agent annotate --style=info --context "gerrit" "{}"'.format(text)]
+    text = f"- The transformed code used in this pipeline can be found under https://{host}-review.googlesource.com/q/{commit}" \
+           f"\n- Fetch the source with `git fetch https://{host}.googlesource.com/bazel {commit}  && git checkout FETCH_HEAD`"
+    commands = ["buildkite-agent annotate --style=info '{}' --context 'gerrit'".format(text)]
     pipeline_steps.append(
         create_step(
             label=":pipeline: Print information about Gerrit Review Link",
