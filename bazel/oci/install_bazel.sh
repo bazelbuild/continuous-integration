cc8dda7e3c7e80359202370df4f29fcf7e6d4149
Clement <clement.jean@epitech.eu>
2024-09-03 01:36:45 -0700

    feat: multi-arch and multi-os build

--- bazel/oci/install_bazel.sh
+++ bazel/oci/install_bazel.sh
@@ -6,13 +6,13 @@ curl \
     --fail \
     --location \
     --remote-name \
-    "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-linux-x86_64"
+    "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${TARGETOS}-${TARGETARCH}"
 
 curl \
     --fail \
     --location \
-    "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-linux-x86_64.sha256" \
+    "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-${TARGETOS}-${TARGETARCH}.sha256" \
     | sha256sum --check
 
-mv "bazel-${BAZEL_VERSION}-linux-x86_64" bazel
+mv "bazel-${BAZEL_VERSION}-${TARGETOS}-${TARGETARCH}" bazel
 chmod +x bazel
