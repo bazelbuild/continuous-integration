3bc086108ba5fa03c03925efa68098537cd3c2ca
Yun Peng <pcloudy@google.com>
2024-10-15 16:56:00 +0200

    Add Dockerfile for ubuntu2404 (#2055)
    Other changes:
    - Removed building ubuntu1604
    - Installed vim on ubuntu2004, ubuntu2204 and ubuntu2404
    - Updated the build.sh and push.sh to avoid centos since it's no longer
      buildable.
    (https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
      is 404)

--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -15,24 +15,23 @@ case $(git symbolic-ref --short HEAD) in
 esac
 
 # Containers used by Bazel CI
-docker push "gcr.io/$PREFIX/centos7" &
-docker push "gcr.io/$PREFIX/centos7-java8" &
-docker push "gcr.io/$PREFIX/centos7-java11" &
-docker push "gcr.io/$PREFIX/centos7-java11-devtoolset10" &
-docker push "gcr.io/$PREFIX/centos7-releaser" &
+# docker push "gcr.io/$PREFIX/centos7" &
+# docker push "gcr.io/$PREFIX/centos7-java11" &
+# docker push "gcr.io/$PREFIX/centos7-java11-devtoolset10" &
+# docker push "gcr.io/$PREFIX/centos7-releaser" &
 docker push "gcr.io/$PREFIX/debian10-java11" &
 docker push "gcr.io/$PREFIX/debian11-java17" &
-docker push "gcr.io/$PREFIX/ubuntu1604-bazel-java8" &
-docker push "gcr.io/$PREFIX/ubuntu1604-java8" &
 docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
 docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
-docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
 docker push "gcr.io/$PREFIX/ubuntu2004" &
 docker push "gcr.io/$PREFIX/ubuntu2204-java17" &
+docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
 docker push "gcr.io/$PREFIX/ubuntu2204-bazel-java17" &
 docker push "gcr.io/$PREFIX/ubuntu2204" &
+docker push "gcr.io/$PREFIX/ubuntu2404" &
+docker push "gcr.io/$PREFIX/ubuntu2404-kythe" &
 docker push "gcr.io/$PREFIX/fedora39-java17" &
 docker push "gcr.io/$PREFIX/fedora39-bazel-java17" &
 docker push "gcr.io/$PREFIX/fedora40-java21" &
