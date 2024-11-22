f5dafb8a6791c4679224cc7fcca061aabebdcce0
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-11-22 11:28:31 +0100

    Don't modify the environment when executing a CI command
    `os.environ` does not contain certain special environment variables on Windows (e.g. `=C:`) that are present when executing from `cmd.exe`.

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -2645,7 +2645,6 @@ def execute_command(
         args,
         shell=shell,
         check=fail_if_nonzero,
-        env=os.environ,
         cwd=cwd,
         errors="replace",
         stdout=(
