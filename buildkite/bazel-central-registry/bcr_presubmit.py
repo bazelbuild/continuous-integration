49ac84bcfb00e1044a39cf0ea69ef722992b3f67
Yun Peng <pcloudy@google.com>
2025-04-04 11:42:12 +0200

    Fix typo in bcr_presubmit.py

--- buildkite/bazel-central-registry/bcr_presubmit.py
+++ buildkite/bazel-central-registry/bcr_presubmit.py
@@ -72,7 +72,7 @@ def get_target_modules():
     for line in output.decode("utf-8").split():
         s = re.match(r"modules\/([^\/]+)\/([^\/]+)\/", line)
         if s:
-            modules.add(s.group())
+            modules.add(s.groups())
 
     return sorted(modules)
 
