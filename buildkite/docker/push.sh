61506120daac802519e9626da209c6754c0c8386
Florian Weikert <fwe@google.com>
2025-04-23 15:18:35 +0200

    Testing (#2251)

--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -19,21 +19,21 @@ docker push "gcr.io/$PREFIX/rockylinux8" &
 docker push "gcr.io/$PREFIX/rockylinux8-java11" &
 docker push "gcr.io/$PREFIX/rockylinux8-java11-devtoolset10" &
 docker push "gcr.io/$PREFIX/rockylinux8-releaser" &
-# docker push "gcr.io/$PREFIX/debian10-java11" &
-# docker push "gcr.io/$PREFIX/debian11-java17" &
-# docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
-# docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
-# docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
-# docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
-# docker push "gcr.io/$PREFIX/ubuntu2004" &
-# docker push "gcr.io/$PREFIX/ubuntu2204-java17" &
-# docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
-# docker push "gcr.io/$PREFIX/ubuntu2204-bazel-java17" &
-# docker push "gcr.io/$PREFIX/ubuntu2204" &
-# docker push "gcr.io/$PREFIX/ubuntu2404" &
-# docker push "gcr.io/$PREFIX/ubuntu2404-kythe" &
-# docker push "gcr.io/$PREFIX/fedora39-java17" &
-# docker push "gcr.io/$PREFIX/fedora39-bazel-java17" &
-# docker push "gcr.io/$PREFIX/fedora40-java21" &
-# docker push "gcr.io/$PREFIX/fedora40-bazel-java21" &
+docker push "gcr.io/$PREFIX/debian10-java11" &
+docker push "gcr.io/$PREFIX/debian11-java17" &
+docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
+docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
+docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
+docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
+docker push "gcr.io/$PREFIX/ubuntu2004" &
+docker push "gcr.io/$PREFIX/ubuntu2204-java17" &
+docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
+docker push "gcr.io/$PREFIX/ubuntu2204-bazel-java17" &
+docker push "gcr.io/$PREFIX/ubuntu2204" &
+docker push "gcr.io/$PREFIX/ubuntu2404" &
+docker push "gcr.io/$PREFIX/ubuntu2404-kythe" &
+docker push "gcr.io/$PREFIX/fedora39-java17" &
+docker push "gcr.io/$PREFIX/fedora39-bazel-java17" &
+docker push "gcr.io/$PREFIX/fedora40-java21" &
+docker push "gcr.io/$PREFIX/fedora40-bazel-java21" &
 wait
