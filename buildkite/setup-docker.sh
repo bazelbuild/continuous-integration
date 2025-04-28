515d30aca982a386f61688caf123f8b056dcd31e
Florian Weikert <fwe@google.com>
2025-04-28 20:58:43 +0200

    Fix Docker arch

--- buildkite/setup-docker.sh
+++ buildkite/setup-docker.sh
@@ -107,7 +107,7 @@ EOF
 
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo \
-      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   apt-get -y update
