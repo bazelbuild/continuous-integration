bazelbuild/continuous-integration

commit 858b77bb991ff0b3fd786bb768e564e0830f5af0

Fix downloading bazel-diff (#2368)
```
curl -q --noproxy -sSL https://github.com/Tinder/bazel-diff/releases/download/8.1.1/bazel-diff_deploy.jar -o /tmp/tmpmn45v_an/bazel-diff.jar
Running bazel-diff for bbf65aea398c490db92a77d7ec077d9a623ce3ff and ddd75b9ace6e3709c697214417e87d0aada0fcb5
java -jar /tmp/tmpmn45v_an/bazel-diff.jar generate-hashes --excludeExternalTargets -w /tmp/tmpmn45v_an/bbf65aea398c490db92a77d7ec077d9a623ce3ff -b bazel --bazelCommandOptions=--lockfile_mode=off /tmp/tmpmn45v_an/before.json
buildkite-agent annotate --style=warning --context 'diff_failed' This build runs all test targets even though `USE_BAZEL_DIFF` is set since bazel-diff failed with an error:
...
Error: Invalid or corrupt jarfile /tmp/tmpmn45v_an/bazel-diff.jar
```


https://buildkite.com/bazel/google-bazel-presubmit/builds/95512#019938cc-8f07-40f4-89e8-c930718e5234

buildkite/bazelci.py
@@ -1111,7 +1111,7 @@ def calculate_flags(task_config, task_config_key, action_key, tmpdir, test_env_v
     # at older commits.
     if is_linux() and is_64_bit():
         flags += ["--linkopt=-Wl,--no-fix-cortex-a53-843419", "--host_linkopt=-Wl,--no-fix-cortex-a53-843419"]
-    
+
     return flags, json_profile_out, capture_corrupted_outputs_dir
 
 
@@ -2477,7 +2477,7 @@ def extract_archive(archive_path, dest_dir, strip_top_level_dir):
 def download_file(url, dest_dir, dest_filename):
     local_path = os.path.join(dest_dir, dest_filename)
     try:
-        execute_command(["curl", "-q", "--noproxy", "-sSL", url, "-o", local_path], capture_stderr=True)
+        execute_command(["curl", "-q", "-sSL", "--noproxy", "*", url, "-o", local_path], capture_stderr=True)
     except subprocess.CalledProcessError as ex:
         raise BuildkiteInfraException("Failed to download {}: {}\n{}".format(url, ex, ex.stderr))
     return local_path
@@ -3225,11 +3225,11 @@ def runner_step(
 
 
 def fetch_bazelcipy_command():
-    return "curl -q --noproxy -sS {0}?{1} -o bazelci.py".format(SCRIPT_URL, int(time.time()))
+    return "curl -q --noproxy * -sS {0}?{1} -o bazelci.py".format(SCRIPT_URL, int(time.time()))
 
 
 def fetch_aggregate_incompatible_flags_test_result_command():
-    return "curl -q --noproxy -sS {0} -o aggregate_incompatible_flags_test_result.py".format(
+    return "curl -q --noproxy * -sS {0} -o aggregate_incompatible_flags_test_result.py".format(
         AGGREGATE_INCOMPATIBLE_TEST_RESULT_URL
     )
 
@@ -3506,7 +3506,7 @@ def fetch_incompatible_flags():
     output = subprocess.check_output(
         [
             # Query for open issues with "incompatible-change" and "migration-ready" label.
-            "curl", "-q", "--noproxy", "-sS",
+            "curl", "-q", "--noproxy", "*", "-sS",
             "https://api.github.com/search/issues?per_page=100&q=repo:bazelbuild/bazel+label:incompatible-change+label:migration-ready+state:open",
         ]
     ).decode("utf-8")

commit 0ad4394d5ad6216131bddc5b53557ce0e6008d3d

Fix curl --noproxy arg
The bug was introduced in https://github.com/bazelbuild/continuous-integration/commit/858b77bb991ff0b3fd786bb768e564e0830f5af0

buildkite/bazelci.py
@@ -1670,7 +1670,7 @@ def download_bazelci_agent(dest_dir):
     name = "bazelci-agent-{}-{}".format(version, postfix)
     url = "https://github.com/{}/releases/download/agent-{}/{}".format(repo, version, name)
     path = os.path.join(dest_dir, "bazelci-agent.exe" if is_windows() else "bazelci-agent")
-    execute_command(["curl", "-q", "--noproxy", "*", "-sSL", url, "-o", path])
+    execute_command(["curl", "-q", "--noproxy", "'*'", "-sSL", url, "-o", path])
     st = os.stat(path)
     os.chmod(path, st.st_mode | stat.S_IEXEC)
     return path
@@ -2477,7 +2477,7 @@ def extract_archive(archive_path, dest_dir, strip_top_level_dir):
 def download_file(url, dest_dir, dest_filename):
     local_path = os.path.join(dest_dir, dest_filename)
     try:
-        execute_command(["curl", "-q", "-sSL", "--noproxy", "*", url, "-o", local_path], capture_stderr=True)
+        execute_command(["curl", "-q", "-sSL", "--noproxy", "'*'", url, "-o", local_path], capture_stderr=True)
     except subprocess.CalledProcessError as ex:
         raise BuildkiteInfraException("Failed to download {}: {}\n{}".format(url, ex, ex.stderr))
     return local_path
@@ -3225,11 +3225,11 @@ def runner_step(
 
 
 def fetch_bazelcipy_command():
-    return "curl -q --noproxy * -sS {0}?{1} -o bazelci.py".format(SCRIPT_URL, int(time.time()))
+    return "curl -q --noproxy '*' -sS {0}?{1} -o bazelci.py".format(SCRIPT_URL, int(time.time()))
 
 
 def fetch_aggregate_incompatible_flags_test_result_command():
-    return "curl -q --noproxy * -sS {0} -o aggregate_incompatible_flags_test_result.py".format(
+    return "curl -q --noproxy '*' -sS {0} -o aggregate_incompatible_flags_test_result.py".format(
         AGGREGATE_INCOMPATIBLE_TEST_RESULT_URL
     )
 
@@ -3506,7 +3506,7 @@ def fetch_incompatible_flags():
     output = subprocess.check_output(
         [
             # Query for open issues with "incompatible-change" and "migration-ready" label.
-            "curl", "-q", "--noproxy", "*", "-sS",
+            "curl", "-q", "--noproxy", "'*'", "-sS",
             "https://api.github.com/search/issues?per_page=100&q=repo:bazelbuild/bazel+label:incompatible-change+label:migration-ready+state:open",
         ]
     ).decode("utf-8")