50e22ff8a7e99111755b58366ccedf0c6fd25642
Yun Peng <pcloudy@google.com>
2026-02-05 16:34:02 +0100

    Add support for Debian 12 and 13

--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -22,6 +22,8 @@ docker push "gcr.io/$PREFIX/rockylinux8-java11-devtoolset10" &
 docker push "gcr.io/$PREFIX/rockylinux8-releaser" &
 docker push "gcr.io/$PREFIX/debian10-java11" &
 docker push "gcr.io/$PREFIX/debian11-java17" &
+docker push "gcr.io/$PREFIX/debian12" &
+docker push "gcr.io/$PREFIX/debian13" &
 docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
 docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
 docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
