f81429af72b4c6f57c9a4985c2fd185de48084b4
Yun Peng <pcloudy@google.com>
2026-02-09 14:36:13 +0000

    Only push debian 13

--- buildkite/docker/push.sh
+++ buildkite/docker/push.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 case $(git symbolic-ref --short HEAD) in
-    master)
+    fix-docker-build)
         PREFIX="bazel-public"
         ;;
     testing)
@@ -15,30 +15,30 @@ case $(git symbolic-ref --short HEAD) in
 esac
 
 # Containers used by Bazel CI
-docker push "gcr.io/$PREFIX/rockylinux8" &
-docker push "gcr.io/$PREFIX/rockylinux8-java8" &
-docker push "gcr.io/$PREFIX/rockylinux8-java11" &
-docker push "gcr.io/$PREFIX/rockylinux8-java11-devtoolset10" &
-docker push "gcr.io/$PREFIX/rockylinux8-releaser" &
-docker push "gcr.io/$PREFIX/debian10-java11" &
-docker push "gcr.io/$PREFIX/debian11-java17" &
-docker push "gcr.io/$PREFIX/debian12" &
+# docker push "gcr.io/$PREFIX/rockylinux8" &
+# docker push "gcr.io/$PREFIX/rockylinux8-java8" &
+# docker push "gcr.io/$PREFIX/rockylinux8-java11" &
+# docker push "gcr.io/$PREFIX/rockylinux8-java11-devtoolset10" &
+# docker push "gcr.io/$PREFIX/rockylinux8-releaser" &
+# docker push "gcr.io/$PREFIX/debian10-java11" &
+# docker push "gcr.io/$PREFIX/debian11-java17" &
+# docker push "gcr.io/$PREFIX/debian12" &
 docker push "gcr.io/$PREFIX/debian13" &
-docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
-docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
-docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
-docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
-docker push "gcr.io/$PREFIX/ubuntu2004" &
-docker push "gcr.io/$PREFIX/ubuntu2204-java17" &
-docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
-docker push "gcr.io/$PREFIX/ubuntu2204-bazel-java17" &
-docker push "gcr.io/$PREFIX/ubuntu2204" &
-docker push "gcr.io/$PREFIX/ubuntu2404" &
-docker push "gcr.io/$PREFIX/ubuntu2404-kythe" &
-docker push "gcr.io/$PREFIX/fedora39-java17" &
-docker push "gcr.io/$PREFIX/fedora39-bazel-java17" &
-docker push "gcr.io/$PREFIX/fedora40-java21" &
-docker push "gcr.io/$PREFIX/fedora40-bazel-java21" &
-docker push "gcr.io/$PREFIX/fedora43-java25" &
-docker push "gcr.io/$PREFIX/fedora43-bazel-java25" &
+# docker push "gcr.io/$PREFIX/ubuntu1804-bazel-java11" &
+# docker push "gcr.io/$PREFIX/ubuntu1804-java11" &
+# docker push "gcr.io/$PREFIX/ubuntu2004-bazel-java11" &
+# docker push "gcr.io/$PREFIX/ubuntu2004-java11" &
+# docker push "gcr.io/$PREFIX/ubuntu2004" &
+# docker push "gcr.io/$PREFIX/ubuntu2204-java17" &
+# docker push "gcr.io/$PREFIX/ubuntu2204-kythe" &
+# docker push "gcr.io/$PREFIX/ubuntu2204-bazel-java17" &
+# docker push "gcr.io/$PREFIX/ubuntu2204" &
+# docker push "gcr.io/$PREFIX/ubuntu2404" &
+# docker push "gcr.io/$PREFIX/ubuntu2404-kythe" &
+# docker push "gcr.io/$PREFIX/fedora39-java17" &
+# docker push "gcr.io/$PREFIX/fedora39-bazel-java17" &
+# docker push "gcr.io/$PREFIX/fedora40-java21" &
+# docker push "gcr.io/$PREFIX/fedora40-bazel-java21" &
+# docker push "gcr.io/$PREFIX/fedora43-java25" &
+# docker push "gcr.io/$PREFIX/fedora43-bazel-java25" &
 wait
