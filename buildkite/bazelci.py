1024798bdf90d8d52cc3b39ab813cb47ef85798a
deepalak56 <deepalakshmib@google.com>
2026-02-10 11:51:55 +0530

    moving back from disabled to downstream pipeline

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -152,7 +152,6 @@ DOWNSTREAM_PROJECTS_PRODUCTION = {
     "Buildfarm": {
         "git_repository": "https://github.com/buildfarm/buildfarm.git",
         "pipeline_slug": "buildfarm-farmer",
-        "disabled_reason": "https://github.com/buildfarm/buildfarm/issues/2396",
     },
     "Buildtools": {
         "git_repository": "https://github.com/bazelbuild/buildtools.git",
