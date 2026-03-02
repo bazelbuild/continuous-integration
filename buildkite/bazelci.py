30af41cfe77ba5c8ab2c80809a6c866c208a4f15
salmasamy <salma.al.emam96@gmail.com>
2026-03-02 13:24:09 +0100

    stop printing

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -702,7 +702,6 @@ gwD6RBL0qz1PFfg7Zw==
         params = params + [("per_page", "100")]
         params_str = "".join("&{}={}".format(k, v) for k, v in params)
         next_url = "{}?access_token={}{}".format(url, self._token, params_str)
-        eprint("url: {}".format(next_url))
 
         all_items = []
         while next_url:
