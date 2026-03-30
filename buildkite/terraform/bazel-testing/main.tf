0143ba478a19636ed0160c6957627c2113593d02
salmasamy <salma.al.emam96@gmail.com>
2026-03-30 12:30:03 +0200

    Update comment

--- buildkite/terraform/bazel-testing/main.tf
+++ buildkite/terraform/bazel-testing/main.tf
@@ -12,7 +12,8 @@ terraform {
   }
 }
 
-# This variable name must remain 'buildkite_api_token' for Terraform to recognize it (e.g. from TF_VAR_buildkite_api_token)
+# This variable name must remain 'buildkite_api_token' for Terraform to
+# be able to recognize it (e.g. from TF_VAR_buildkite_api_token)
 variable "buildkite_api_token" {
   description = "The API token used to authenticate with Buildkite"
   type        = string
