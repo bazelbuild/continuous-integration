bdf46b745e347c5ce337e0b2a32d892f5b9eb5ab
Florian Weikert <fwe@google.com>
2026-04-01 21:06:03 +0200

    Test link in step label

--- buildkite/bazelci.py
+++ buildkite/bazelci.py
@@ -3201,7 +3201,7 @@ def create_buildifier_step(buildifier_config):
 
 def create_check_docs_step():
     return create_docker_step(
-        ":passport_control: Check Docs",
+        ":passport_control: Check Docs [md link](http://google.com) / <a href='http://google.com'>html link</a>",
         image=MINTLIFY_DOCKER_IMAGE,
         # TODO: make env variables configurable via yaml.
         additional_env_vars={
