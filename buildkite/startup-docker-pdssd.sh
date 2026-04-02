853bd2274510bd2e23a7b92e71aeb4dc8a45b66f
salmasamy <salma.al.emam96@gmail.com>
2026-04-02 14:58:35 +0200

    Revert "Add hooks to capture bootstrap durations"
    This reverts commit 73c33284b3c4d4fa741641dd4fc870b69ba2b252.

--- buildkite/startup-docker-pdssd.sh
+++ buildkite/startup-docker-pdssd.sh
@@ -84,36 +84,6 @@ set -euo pipefail
 export BUILDKITE_ARTIFACT_UPLOAD_DESTINATION="gs://${ARTIFACT_BUCKET}/\${BUILDKITE_JOB_ID}"
 EOF
 
-### 1. Capture Job Start Time
-cat > /etc/buildkite-agent/hooks/pre-checkout <<'EOF'
-#!/bin/bash
-NOW=$(date +%s%3N)
-buildkite-agent env set "JOB_START_TIME=$NOW"
-EOF
-
-### 2. Calculate Checkout Duration
-cat > /etc/buildkite-agent/hooks/post-checkout <<'EOF'
-#!/bin/bash
-CHECKOUT_END_TIME=$(date +%s%3N)
-START_TIME=$(buildkite-agent env get JOB_START_TIME)
-START_TIME=${START_TIME:-0}
-DIFF=$((CHECKOUT_END_TIME - START_TIME))
-CHECKOUT_DURATION_S=$(printf "%d.%03d" $((DIFF / 1000)) $((DIFF % 1000)))
-buildkite-agent env set "CHECKOUT_END_TIME=${CHECKOUT_END_TIME}"
-buildkite-agent env set "CHECKOUT_DURATION_S=${CHECKOUT_DURATION_S}"
-EOF
-
-### 3. Calculate Prep Duration
-cat > /etc/buildkite-agent/hooks/pre-command <<'EOF'
-#!/bin/bash
-PREP_END_TIME=$(date +%s%3N)
-CHECKOUT_END=$(buildkite-agent env get CHECKOUT_END_TIME)
-CHECKOUT_END=${CHECKOUT_END:-0}
-DIFF=$((PREP_END_TIME - CHECKOUT_END))
-PREP_DURATION_S=$(printf "%d.%03d" $((DIFF / 1000)) $((DIFF % 1000)))
-buildkite-agent env set "PREP_DURATION_S=${PREP_DURATION_S}"
-EOF
-
 ### Fix permissions of the Buildkite agent configuration files and hooks.
 chmod 0400 /etc/buildkite-agent/buildkite-agent.cfg
 chmod 0500 /etc/buildkite-agent/hooks/*
