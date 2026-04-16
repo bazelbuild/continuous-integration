4c3f292e8f2f957c688cdd89711a131730250439
Yun Peng <pcloudy@google.com>
2026-04-16 09:46:53 +0000

    Delete archived pipelines causing Terraform failures

--- buildkite/terraform/bazel-trusted/pipelines_bazel.tf
+++ buildkite/terraform/bazel-trusted/pipelines_bazel.tf
@@ -440,59 +440,7 @@ resource "buildkite_pipeline" "bazel-arm64" {
   }
 }
 
-resource "buildkite_pipeline" "bazel-bench-master-report-deprecated" {
-  name           = "Bazel Bench Master Report - Deprecated"
-  repository     = "https://github.com/bazelbuild/bazel-bench.git"
-  description    = "Generates the daily combined performance report."
-  default_branch = "master"
-  steps = templatefile("pipeline.yml.tpl", {
-    envs = {},
-    steps = {
-      commands = [
-        "bazel run //report:generate_master_report -- --date=\"$(date --date yesterday +%Y-%m-%d)\"  --storage_bucket=perf.bazel.build --bigquery_table='bazel-public:bazel_bench.bazel_bench_daily' --upload_report=True",
-        "gsutil cp gs://perf.bazel.build/all/\"$(date --date yesterday +%Y/%m/%d)\"/report.html gs://perf.bazel.build/all/report_latest.html"
-      ],
-      label = ":pipeline:",
-      retry = true
-    }
-  })
-  allow_rebuilds             = true
-  cancel_intermediate_builds = false
-  skip_intermediate_builds   = false
-  tags                       = []
-  branch_configuration       = null
-  cluster_id                 = null
-  color                      = null
-  default_team_id            = null
-  emoji                      = null
-  pipeline_template_id       = null
-  provider_settings = {
-    trigger_mode                                  = "code"
-    build_branches                                = false
-    build_pull_requests                           = true
-    build_tags                                    = false
-    build_pull_request_forks                      = false
-    build_pull_request_ready_for_review           = false
-    build_pull_request_labels_changed             = false
-    build_pull_request_base_branch_changed        = false
-    prefix_pull_request_fork_branch_names         = true
-    filter_enabled                                = false
-    filter_condition                              = ""
-    pull_request_branch_filter_enabled            = false
-    pull_request_branch_filter_configuration      = ""
-    publish_commit_status                         = true
-    publish_commit_status_per_step                = false
-    separate_pull_request_statuses                = false
-    publish_blocked_as_pending                    = false
-    cancel_deleted_branch_builds                  = false
-    skip_builds_for_existing_commits              = false
-    skip_pull_request_builds_for_existing_commits = true
-    ignore_default_branch_pull_requests           = false
-    build_merge_group_checks_requested            = false
-    cancel_when_merge_group_destroyed             = false
-    use_merge_group_base_commit_for_git_diff_base = false
-  }
-}
+
 
 resource "buildkite_pipeline" "bazel-release" {
   name           = "Bazel Release"
@@ -614,38 +562,7 @@ resource "buildkite_pipeline" "bazel-bench-culprit-finder" {
   }
 }
 
-resource "buildkite_pipeline" "bazel-bench-nightly-deprecated" {
-  name           = "Bazel Bench Nightly - Deprecated"
-  repository     = "https://github.com/bazelbuild/bazel-bench.git"
-  description    = "Runs bazel-bench every night and records the performance of Bazel for each commits during that day."
-  default_branch = "master"
-  steps = templatefile("pipeline.yml.tpl", {
-    envs = {},
-    steps = {
-      commands = [
-        "curl -sS \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/bazelci.py?$(date +%s)\" -o bazelci.py",
-        "curl -sS \"https://raw.githubusercontent.com/bazelbuild/continuous-integration/master/buildkite/bazel-bench/bazel_bench.py?$(date +%s)\" -o bazel_bench.py",
-        "python3.6 bazel_bench.py --date=\"$(date --date yesterday +%Y-%m-%d)\" --bucket=perf.bazel.build --bigquery_table='bazel-public:bazel_bench.bazel_bench_daily' --bazel_bench_options=\"--runs=7\" --max_commits=7 --update_latest --upload_report"
-      ],
-      label = ":pipeline:",
-      retry = true
-    }
-  })
-  allow_rebuilds             = true
-  cancel_intermediate_builds = false
-  skip_intermediate_builds   = false
-  tags                       = []
-  branch_configuration       = null
-  cluster_id                 = null
-  color                      = null
-  default_team_id            = null
-  emoji                      = null
-  pipeline_template_id       = null
-  provider_settings = {
-    # GitHub activities are disabled for this pipeline
-    trigger_mode = "none"
-  }
-}
+
 
 resource "buildkite_pipeline" "bazel-bench-nightly-test" {
   name           = "Bazel Bench Nightly - Test"
