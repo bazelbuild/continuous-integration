6c7764683d18823a9fb59d5e8febfdefd26cbc05
Florian Weikert <fwe@google.com>
2026-05-21 20:13:00 +0200

    Update Mintlify CLI before every run

--- mintlify/check_docs.sh
+++ mintlify/check_docs.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 
 source "$(dirname "$0")/download_json.sh"
 
+echo "--- Updating Mintlify CLI"
+mint version
+mint update
+mint version
+
 if [[ "$(git config --get remote.origin.url)" == */bazel.git ]]; then
   # Bazel repo -> need to build reference docs and download .json navigation
   echo "--- :bazel::books: Building reference docs"
@@ -28,8 +33,6 @@ else
 fi
 
 echo "+++ :male-detective::books: Checking documentation with Mintlify"
-
-mint --version
 
 # https://www.mintlify.com/docs/installation#validate-documentation-build
 # If validation fails, we annotate the build and exit.
