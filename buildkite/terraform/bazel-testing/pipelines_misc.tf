0459494e6f7fc9d5c24ebda063c7c20fd7a72a22
Ted Xie <tedx@google.com>
2026-08-20 11:16:50 -0400

    Update bazel-testing pipelines
    * Add a buildfarm testing pipeline
    * Remove my custom custom pipeline (it was not necessary)

--- buildkite/terraform/bazel-testing/pipelines_misc.tf
+++ buildkite/terraform/bazel-testing/pipelines_misc.tf
@@ -108,3 +108,45 @@ resource "buildkite_pipeline" "ci-smoke-test" {
   }
 }
 
+resource "buildkite_pipeline" "buildfarm-admin-farmer" {
+  name                       = "Buildfarm Admin :farmer:"
+  repository                 = "https://github.com/buildfarm/bfadmin.git"
+  default_branch             = "main"
+  steps                      = "---\nsteps:\n  - command: \".bazelci/test_bfadmin.sh\"\n    label: \"Build and Test\"\n    agents:\n      - \"queue=default\"\n"
+  allow_rebuilds             = true
+  cancel_intermediate_builds = false
+  skip_intermediate_builds   = false
+  tags                       = []
+  branch_configuration       = null
+  cluster_id                 = null
+  color                      = null
+  default_team_id            = null
+  emoji                      = null
+  pipeline_template_id       = null
+  provider_settings = {
+    trigger_mode                                  = "code"
+    build_branches                                = true
+    build_pull_requests                           = true
+    build_tags                                    = false
+    build_pull_request_forks                      = false
+    build_pull_request_ready_for_review           = false
+    build_pull_request_labels_changed             = false
+    build_pull_request_base_branch_changed        = false
+    prefix_pull_request_fork_branch_names         = true
+    filter_enabled                                = false
+    pull_request_branch_filter_enabled            = false
+    publish_commit_status                         = true
+    publish_commit_status_per_step                = false
+    separate_pull_request_statuses                = false
+    publish_blocked_as_pending                    = true
+    cancel_deleted_branch_builds                  = false
+    skip_builds_for_existing_commits              = false
+    skip_pull_request_builds_for_existing_commits = true
+    ignore_default_branch_pull_requests           = false
+    build_merge_group_checks_requested            = false
+    cancel_when_merge_group_destroyed             = false
+    use_merge_group_base_commit_for_git_diff_base = false
+  }
+}
+
+
