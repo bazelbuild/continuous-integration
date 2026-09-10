7907b1dcce06f09c6be3722de15eafa3b56a3d1d
Ted Xie <tedx@google.com>
2026-09-10 15:11:13 -0400

    More cleanups
    * Delete ubuntu 16
    * Add ubuntu2204.json (same as 1804.json, which was the same as 1604)
    * Remove outdated cpp_env json files

--- rbe-configs/data.py
+++ rbe-configs/data.py
@@ -8,7 +8,7 @@ configs = [
       },
       {
           'toolchain_name': 'ubuntu2204',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       }
     ]
   },
@@ -17,7 +17,7 @@ configs = [
     "containers": [
       {
           'toolchain_name': 'ubuntu2204',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       }
     ]
   },
@@ -26,7 +26,7 @@ configs = [
     "containers": [
       {
           'toolchain_name': 'ubuntu2204',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       }
     ]
   },
@@ -35,7 +35,7 @@ configs = [
     "containers": [
       {
           'toolchain_name': 'ubuntu2204',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       }
     ]
   },
@@ -44,115 +44,11 @@ configs = [
     "containers": [
       {
           'toolchain_name': 'ubuntu2204-bazel-java17',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       },
       {
           'toolchain_name': 'ubuntu2204-java17',
-          'cpp_env_json': 'cpp_env/ubuntu2004.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "5.2.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "5.1.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "5.0.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "4.2.2",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "4.2.1",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "4.2.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "4.1.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-    ]
-  },
-  {
-    "bazel_version": "4.0.0",
-    "containers": [
-      {
-          'toolchain_name': 'ubuntu1604-bazel-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
-      },
-      {
-          'toolchain_name': 'ubuntu1604-java8',
-          'cpp_env_json': 'cpp_env/ubuntu1604.json'
+          'cpp_env_json': 'cpp_env/ubuntu2204.json'
       },
     ]
   },
